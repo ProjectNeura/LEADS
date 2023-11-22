1f92cc261edb9426dafa470d5563407de5c76c66
ATATC <futerry@outlook.com>
2023-11-21 20:48:06 -0500

    Routine updates.

--- test_comm_server.py
+++ test_comm_server.py
@@ -1,3 +1,4 @@
+from leads.comm import Connection
 from leads.comm.prototype import Service
 from leads.comm.server import *
 
@@ -5,6 +6,9 @@ from leads.comm.server import *
 class CustomCallback(Callback):
     def on_fail(self, service: Service, error: Exception):
         print(error)
+
+    def on_connect(self, service: Service, connection: Connection):
+        print(f"Connected to {connection}")
 
     def on_receive(self, service: Service, msg: bytes):
         print(msg)
