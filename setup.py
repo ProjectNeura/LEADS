ad359ba89eae6f2bb46cec5eaf6b61c791f2382c
ATATC <futerry@outlook.com>
2023-11-23 11:43:50 -0500

    Bug fixed.

--- setup.py
+++ setup.py
@@ -1,67 +1,19 @@
 from setuptools import find_packages, setup
 
 
-def find_certain_packages(prefix: str) -> list[str]:
-    return list(map(lambda p: prefix + "." + p, find_packages(prefix)))
-
-
-SHORT_DESCRIPTION = "Lightweight Embedded Assisted Driving System"
 with open("./README.md", "r") as f:
     LONG_DESCRIPTION = f.read()
-AUTHOR_EMAIL = "central@projectneura.org"
 
 setup(
     name="leads",
     version="0.0.1",
     author="ProjectNeura",
-    author_email=AUTHOR_EMAIL,
-    description=SHORT_DESCRIPTION,
+    author_email="central@projectneura.org",
+    description="Lightweight Embedded Assisted Driving System",
     license='Apache License 2.0',
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/ProjectNeura/LEADS",
-    packages=find_certain_packages("leads"),
+    packages=find_packages(),
     install_requires=[]
-)
-
-setup(
-    name="leads-dashboard",
-    version="0.0.1",
-    author="ProjectNeura",
-    author_email=AUTHOR_EMAIL,
-    description=SHORT_DESCRIPTION,
-    license='Apache License 2.0',
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ProjectNeura/LEADS",
-    packages=find_certain_packages("leads_dashboard"),
-    install_requires=["dearpygui", "leads"]
-)
-
-setup(
-    name="leads-emulation",
-    version="0.0.1",
-    author="ProjectNeura",
-    author_email=AUTHOR_EMAIL,
-    description=SHORT_DESCRIPTION,
-    license='Apache License 2.0',
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ProjectNeura/LEADS",
-    packages=find_certain_packages("leads_emulation"),
-    install_requires=["leads"]
-)
-
-setup(
-    name="leads-vec",
-    version="0.0.1",
-    author="ProjectNeura",
-    author_email=AUTHOR_EMAIL,
-    description=SHORT_DESCRIPTION,
-    license='Apache License 2.0',
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ProjectNeura/LEADS",
-    packages=find_certain_packages("leads_vec"),
-    install_requires=["keyboard", "leads", "leads-dashboard", "leads-emulation"]
 )
