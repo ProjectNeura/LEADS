cb858488dbdb4307bfb812e83c1ddb736af094cd
ATATC <futerry@outlook.com>
2023-11-28 10:03:06 -0500

    Bug fixed: `Controller` is undefined in `leads_emulation`.

--- leads_vec/__main__.py
+++ leads_vec/__main__.py
@@ -1,9 +1,8 @@
 from sys import argv as _argv, exit as _exit
 
-
 if __name__ == '__main__':
     try:
-        from leads_emulation import Random as Controller
+        from leads_emulation import SRWRandom as Controller
     except ImportError:
         raise EnvironmentError("At least one adapter has to be installed")
     if "remote" in _argv:
