d7cae9c282d21d79b563bbdc9378d4a6450ae574
ATATC <futerry@outlook.com>
2023-12-15 09:20:49 -0500

    Routine updates.

--- leads_vec/config.py
+++ leads_vec/config.py
@@ -1,5 +1,5 @@
 from json import load as _load
-from typing import TextIO as _TextIO, Any as _Any, Self as _Self
+from typing import TextIO as _TextIO, Any as _Any
 
 
 class Config(object):
@@ -11,6 +11,7 @@ class Config(object):
         self.comm_addr: str = "127.0.0.1"
         self.comm_port: int = 16900
         self.data_dir: str = "./data"
+        self.refresh()
 
     def __getitem__(self, name: str) -> _Any | None:
         return self.get(name)
@@ -18,20 +19,40 @@ class Config(object):
     def __getattr__(self, name: str) -> _Any | None:
         return self.get(name)
 
+    def __setitem__(self, name: str, value: _Any) -> None:
+        self._d[name] = value
+        self.refresh()
+
+    def load(self, d: dict[str, _Any]) -> None:
+        """
+        Load a new dictionary and refresh.
+        :param d: the dictionary to load
+        """
+        self._d = d
+        self.refresh()
+
     def get(self, name: str, default: _Any | None = None) -> _Any:
+        """
+        Get the value of a given name from the dictionary.
+        :param name: dictionary key
+        :param default: default value if the value does not exist
+        :return: the value if it exists or else the default value
+        """
         if r := self._d.get(name):
             return r
         return default
 
-    def load(self) -> _Self:
+    def refresh(self) -> None:
+        """
+        Write the dictionary into the instance attributes.
+        """
         for name in dir(self):
             if not name.startswith("_") and (v := self.get(name)):
                 self.__setattr__(name, v)
-        return self
 
 
 DEFAULT_CONFIG = Config({})
 
 
 def load_config(file: str | _TextIO) -> Config:
-    return Config(_load(open(file, "r")) if isinstance(file, str) else _load(file)).load()
+    return Config(_load(open(file, "r")) if isinstance(file, str) else _load(file))
