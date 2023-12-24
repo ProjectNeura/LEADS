81652d540c94be118cf0e536c8b9a500d28ae88d
ATATC <futerry@outlook.com>
2023-12-24 15:01:03 -0500

    Version `0.1.0`.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md") as f:
 
 setup(
     name="leads",
-    version="0.0.10",
+    version="0.1.0",
     author="ProjectNeura",
     author_email="central@projectneura.org",
     description="Lightweight Embedded Assisted Driving System",
