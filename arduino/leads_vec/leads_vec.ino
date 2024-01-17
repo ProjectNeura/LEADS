d0d6735d6aa58a2e56107063f0731b714e83c3c0
ATATC <futerry@outlook.com>
2024-01-17 12:23:31 -0500

    Routine updates.

--- arduino/leads_vec/leads_vec.ino
+++ arduino/leads_vec/leads_vec.ino
@@ -1,6 +1,22 @@
+const int PIN_LFWSS = 0;
+const int PIN_RFWSS = 0;
+const int PIN_LRWSS = 0;
+const int PIN_RRWSS = 0;
+const int PIN_MRWSS = 0;
+
 void setup() {
+  pinMode(PIN_LFWSS, INPUT);
+  pinMode(PIN_RFWSS, INPUT);
+  pinMode(PIN_LRWSS, INPUT);
+  pinMode(PIN_RRWSS, INPUT);
+  pinMode(PIN_MRWSS, INPUT);
   Serial.begin(9600);
 }
 
+boolean pulse(int pin) {
+  return digitalRead(pin) == LOW
+}
+
 void loop() {
+  if (pulse(PIN_LFWSS) Serial.println("A");
 }
