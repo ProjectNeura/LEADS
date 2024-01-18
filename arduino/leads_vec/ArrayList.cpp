dd3d5bf369e4b44419237f789d941d0298e63fa7
ATATC <futerry@outlook.com>
2024-01-17 21:32:03 -0500

    Implementing ArrayList in C++.

--- arduino/leads_vec/ArrayList.cpp
+++ arduino/leads_vec/ArrayList.cpp
@@ -3,10 +3,22 @@
 template<typename E>
 ArrayList<E>::ArrayList(int initialCapacity) {
   if (initialCapacity > 0) {
-    this.elementData = new E[initialCapacity];
+    elementData = new E[initialCapacity];
   } else if (initialCapacity == 0) {
-    this.elementData = EMPTY_ELEMENTDATA;
+    elementData = EMPTY_ELEMENTDATA;
   } else {
     throw invalid_argument("Illegal Capacity: " + initialCapacity);
   }
+}
+template<typename E>
+ArrayList<E>::ArrayList() {
+  elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+}
+template<typename E>
+ArrayList<E>::ArrayList(E* initialArray) {
+  if (initialArray.length > 0) elementData = initialArray;
+  else elementData = EMPTY_ELEMENTDATA;
+}
+template<typename E>
+int ArrayList<E>::calculateCapacity(E* elementData, int minCapacity) {
 }
