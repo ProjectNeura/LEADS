4c0f24d76f53609b7cf1334f8b18dbc96dbac104
ATATC <futerry@outlook.com>
2024-01-21 12:24:07 -0500

    Implemented a logger.

--- leads/__init__.py
+++ leads/__init__.py
@@ -2,3 +2,6 @@ from leads.data import *
 from leads.dt import *
 from leads.event import *
 from leads.leads import *
+from leads.logger import Level, Logger as _Logger
+
+L: _Logger = _Logger()
---
+++ leads/logger.py
@@ -0,0 +1,59 @@
+from datetime import datetime as _datetime
+from enum import IntEnum as _IntEnum
+
+
+class Level(_IntEnum):
+    INFO: int = 0x00
+    DEBUG: int = 0x01
+    WARN: int = 0x02
+    ERROR: int = 0x03
+
+
+class Logger(object):
+    REGULAR: int = 0
+    BOLD: int = 1
+    ITALIC: int = 3
+    UNDERLINED: int = 4
+    INVERSE: int = 7
+
+    BLACK: int = 30
+    RED: int = 31
+    GREEN: int = 32
+    YELLOW: int = 33
+    BLUE: int = 34
+    PURPLE: int = 35
+    CYAN: int = 36
+    WHITE: int = 37
+
+    def __init__(self) -> None:
+        self._debug_level: Level = Level.INFO
+
+    def debug_level(self, debug_level: Level | None = None) -> Level | None:
+        if debug_level:
+            self._debug_level = debug_level
+        else:
+            return self._debug_level
+
+    @staticmethod
+    def mark(msg: str, level: Level) -> str:
+        return f"[{repr(level)[1:-1]}] [{_datetime.now()}] {msg}"
+
+    @staticmethod
+    def format(msg: str, font: int, color: int | None, background: int | None) -> str:
+        return f"\033[{font}{f';{color}' if color else ''}{f';{background + 10}' if background else ''}m{msg}\033[0m"
+
+    def print(self, msg: str, level: int) -> None:
+        if self._debug_level <= level:
+            print(msg)
+
+    def info(self, msg: str, f: tuple[int, int | None, int | None] = (REGULAR, None, None)):
+        self.print(self.format(self.mark(msg, Level.INFO), *f), Level.INFO)
+
+    def debug(self, msg: str, f: tuple[int, int | None, int | None] = (REGULAR, YELLOW, None)):
+        self.print(self.format(self.mark(msg, Level.DEBUG), *f), Level.DEBUG)
+
+    def warn(self, msg: str, f: tuple[int, int | None, int | None] = (REGULAR, RED, None)):
+        self.print(self.format(self.mark(msg, Level.WARN), *f), Level.WARN)
+
+    def error(self, msg: str, f: tuple[int, int | None, int | None] = (REGULAR, RED, None)):
+        self.print(self.format(self.mark(msg, Level.ERROR), *f), Level.ERROR)
