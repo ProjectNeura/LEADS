707f309f89b810ce86caf4da062a85d7da1936fb
ATATC <futerry@outlook.com>
2024-01-22 11:48:46 -0500

    Implementing ArrayList in C++.

--- arduino/leads_vec/ArrayList.h
+++ arduino/leads_vec/ArrayList.h
@@ -14,6 +14,14 @@ public:
     ArrayList(E *initialArray);
     int size();
     E *const &toArray();
+    E get(int index);
+    bool add(E element);
+    bool insert(int index, E element);
+    bool contains(E element);
+    int indexOfInRange(E element, int start, int end);
+    int indexOf(E element);
+    int lastIndexOfInRange(E element, int start, int end);
+    int lastIndexOf(E element);
 };
 
 
