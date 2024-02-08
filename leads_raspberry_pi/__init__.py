943cd3cebbb41685237f1c46df1fd0451c4125d6
ATATC <futerry@outlook.com>
2024-02-08 14:55:14 -0500

    Error message improved.

--- leads_raspberry_pi/__init__.py
+++ leads_raspberry_pi/__init__.py
@@ -1,7 +1,7 @@
 from importlib.util import find_spec as _find_spec
 
 if not _find_spec("RPi"):
-    raise ImportError("Please install `RPi.GPIO` to run this module\n>>>pip install RPi.GPIO")
+    raise ImportError("Please install `RPi.GPIO` to run this module\n>>>apt install python3-rpi.gpio")
 if not _find_spec("serial"):
     raise ImportError("Please install `pyserial` to run this module\n>>>pip install pyserial")
 
