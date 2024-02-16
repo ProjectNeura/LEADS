ed0945928934c923d22343a446aa06c76a9b5b03
ATATC <futerry@outlook.com>
2024-02-16 02:53:37 -0500

    Wheel speeds from different wheels are now managed separately.

--- leads_vec/controller.py
+++ leads_vec/controller.py
@@ -1,15 +1,19 @@
+from typing import Optional as _Optional
+
 from leads import L, device, controller, MAIN_CONTROLLER, get_controller, WHEEL_SPEED_CONTROLLER, SRWDataContainer, \
-    DRWDataContainer, DC_MOTOR_CONTROLLER_A
+    DRWDataContainer, DC_MOTOR_CONTROLLER_A, LEFT_FRONT_WHEEL_SPEED_SENSOR, RIGHT_FRONT_WHEEL_SPEED_SENSOR, \
+    CENTER_REAR_WHEEL_SPEED_SENSOR, LEFT_REAR_WHEEL_SPEED_SENSOR, RIGHT_REAR_WHEEL_SPEED_SENSOR
 from leads.comm import Callback, Service, ConnectionBase
 from leads.config import get_config
-from leads_arduino import ArduinoMicro
+from leads_arduino import ArduinoMicro, WheelSpeedSensor
 from leads_gui import Config
 from leads_raspberry_pi import RaspberryPi4B, DCMotorController, VoltageSensor
 
 config = get_config(Config)
 BAUD_RATE: int = config.get("baud_rate", 9600)
 WHEEL_SPEED_CONTROLLER_PORT: str = config.get("wheel_speed_controller_port", "COM3")
-WHEEL_RADIUS: float = config.get("wheel_radius", 159.5)  # 20 inches
+FRONT_WHEEL_RADIUS: float = config.get("front_wheel_radius", 159.5)  # 20 inches
+REAR_WHEEL_RADIUS: float = config.get("rear_wheel_radius", 159.5)  # 20 inches
 THROTTLE_PEDAL_PIN: int = config.get("throttle_pedal_pin", 2)
 BRAKE_PEDAL_PIN: int = config.get("brake_pedal_pin", 3)
 VOLTAGE_SENSOR_PIN: int = config.get("voltage_sensor_pin", 4)
@@ -27,7 +31,7 @@ class WheelSpeedControllerCallback(Callback):
         L.debug("Wheel speed controller connected")
 
     def on_receive(self, service: Service, msg: bytes) -> None:
-        get_controller(WHEEL_SPEED_CONTROLLER).update(float(msg.decode()))
+        get_controller(WHEEL_SPEED_CONTROLLER).update(msg.decode())
 
     def on_fail(self, service: Service, error: Exception) -> None:
         L.error(str(error))
@@ -37,13 +41,46 @@ class WheelSpeedControllerCallback(Callback):
         WHEEL_SPEED_CONTROLLER_PORT, WheelSpeedControllerCallback(), BAUD_RATE
 ))
 class WheelSpeedController(ArduinoMicro):
-    _wheel_speed: float = 0
+    def update(self, data: str) -> None:
+        for d in self.devices():
+            if data.startswith(d.tag()):
+                d.update(float(data[data.find(":") + 1:]))
 
-    def update(self, data: float) -> None:
-        self._wheel_speed = data * WHEEL_RADIUS * .0006
+    def read(self) -> [float, float, float, _Optional[float]]:
+        lfws = self.device(LEFT_FRONT_WHEEL_SPEED_SENSOR).read()
+        rfws = self.device(RIGHT_FRONT_WHEEL_SPEED_SENSOR).read()
+        rws = (self.device(CENTER_REAR_WHEEL_SPEED_SENSOR).read(),) if config.srw_mode else (
+            self.device(LEFT_REAR_WHEEL_SPEED_SENSOR).read(),
+            self.device(RIGHT_REAR_WHEEL_SPEED_SENSOR).read()
+        )
+        return min(lfws, rfws, *rws), (lfws + rfws) * .5, *rws
 
-    def read(self) -> [float, float]:
-        return self._wheel_speed, self._wheel_speed
+
+@device(*((
+        (
+                LEFT_FRONT_WHEEL_SPEED_SENSOR,
+                RIGHT_FRONT_WHEEL_SPEED_SENSOR,
+                CENTER_REAR_WHEEL_SPEED_SENSOR
+        ), WHEEL_SPEED_CONTROLLER, [
+            (FRONT_WHEEL_RADIUS,),
+            (FRONT_WHEEL_RADIUS,),
+            (REAR_WHEEL_RADIUS,)
+        ]
+) if config.srw_mode else (
+        (
+                LEFT_FRONT_WHEEL_SPEED_SENSOR,
+                RIGHT_FRONT_WHEEL_SPEED_SENSOR,
+                LEFT_REAR_WHEEL_SPEED_SENSOR,
+                RIGHT_REAR_WHEEL_SPEED_SENSOR
+        ), WHEEL_SPEED_CONTROLLER, [
+            (FRONT_WHEEL_RADIUS,),
+            (FRONT_WHEEL_RADIUS,),
+            (REAR_WHEEL_RADIUS,),
+            (REAR_WHEEL_RADIUS,)
+        ]
+)))
+class WheelSpeedSensors(WheelSpeedSensor):
+    pass
 
 
 # @device((THROTTLE_PEDAL, BRAKE_PEDAL), MAIN_CONTROLLER, [(THROTTLE_PEDAL_PIN, False), (BRAKE_PEDAL_PIN, True)])
