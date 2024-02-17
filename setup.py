3a0b4ec39496f41ea36fec2f06c369efde48b9df
ATATC <futerry@outlook.com>
2024-02-17 12:30:23 -0500

    Added Python version requirement.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ with open("./README.md") as f:
 setup(
     name="leads",
     version="0.6.1",
+    python_requires=">=3.12",
     author="ProjectNeura",
     author_email="central@projectneura.org",
     description="Lightweight Embedded Assisted Driving System",
