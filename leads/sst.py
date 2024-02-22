8ceb4d4442841cd8979d6ab37ecf4ebe63dc2ee8
ATATC <futerry@outlook.com>
2024-02-21 23:45:56 -0500

    Building a system status tracer. (#59)

--- leads/sst.py
+++ leads/sst.py
@@ -1,6 +1,8 @@
 from typing import Callable as _Callable
 
+from leads.context import Context
 from leads.dt import Device
+from leads.event import SuspensionEvent
 
 
 def mark_system(device: Device, system: str) -> None:
@@ -13,4 +15,10 @@ def read_marked_system(device: Device) -> str | None:
 
 class SystemStatusTracer(object):
     def __init__(self) -> None:
-        self.on_fail: _Callable[[Device, str, ], None]
+        self._context: Context | None = None
+        self.on_fail: _Callable[[Device, SuspensionEvent], None] = lambda _, __: None
+
+    def fail(self, device: Device, e: Exception) -> None:
+        if not (system := read_marked_system(device)):
+            raise RuntimeWarning("System not marked for device " + str(device))
+        self.on_fail(device, SuspensionEvent(self._context, system, str(e)))
