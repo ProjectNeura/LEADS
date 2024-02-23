0c21b4821fb70f2892374c1d85855c27c12f5362
ATATC <futerry@outlook.com>
2024-02-23 11:02:22 -0500

    Implemented https://github.com/ProjectNeura/LEADS/issues/59#issuecomment-1960086412.

--- leads/sft.py
+++ leads/sft.py
@@ -6,11 +6,13 @@ from leads.event import SuspensionEvent
 from leads.logger import L
 
 
-def mark_system(device: Device, system: str) -> None:
-    device.__setattr__("__device_system__", system)
+def mark_system(device: Device, system: str, *related: str) -> None:
+    if hasattr(device, "__device_system__"):
+        device.__setattr__("__device_system__", device.__getattribute__("__device_system__") + [system, *related])
+    device.__setattr__("__device_system__", [system])
 
 
-def read_marked_system(device: Device) -> str | None:
+def read_marked_system(device: Device) -> list[str] | None:
     return device.__getattribute__("__device_system__") if hasattr(device, "__device_system__") else None
 
 
@@ -21,16 +23,18 @@ class SystemFailureTracer(object):
         self.on_recover: _Callable[[Device, SuspensionEvent], None] = lambda _, __: None
 
     def fail(self, device: Device, e: Exception) -> None:
-        if not (system := read_marked_system(device)):
+        if not (systems := read_marked_system(device)):
             raise RuntimeWarning("System not marked for device " + str(device))
-        self.on_fail(device, SuspensionEvent(self._context, system, str(e)))
-        L.error(f"{system} error: {e}")
+        for system in systems:
+            self.on_fail(device, SuspensionEvent(self._context, system, str(e)))
+            L.error(f"{system} error: {e}")
 
     def recover(self, device: Device) -> None:
-        if not (system := read_marked_system(device)):
+        if not (systems := read_marked_system(device)):
             raise RuntimeWarning("System not marked for device " + str(device))
-        self.on_recover(device, SuspensionEvent(self._context, system, "Recovered"))
-        L.info(system + " recovered")
+        for system in systems:
+            self.on_recover(device, SuspensionEvent(self._context, system, "Recovered"))
+            L.info(system + " recovered")
 
 
 SFT = SystemFailureTracer()
