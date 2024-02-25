ddd98a25a20b98c4547e77db1ef75c3ab63ff143
ATATC <futerry@outlook.com>
2024-02-25 12:19:19 -0500

    Bug fixed.

--- setup.py
+++ setup.py
@@ -16,17 +16,7 @@ setup(
     url="https://github.com/ProjectNeura/LEADS",
     packages=find_packages(),
     package_data={
-        "leads_gui": [
-            "assets/leads-theme.json", "assets/icons/battery-black.png", "assets/icons/battery-red.png",
-            "assets/icons/battery-white.png", "assets/icons/brake-black.png", "assets/icons/brake-red.png",
-            "assets/icons/brake-white.png", "assets/icons/ecs-black.png", "assets/icons/ecs-red.png",
-            "assets/icons/ecs-white.png", "assets/icons/engine-black.png", "assets/icons/engine-red.png",
-            "assets/icons/engine-white.png", "assets/icons/high-beam-black.png", "assets/icons/high-beam-red.png",
-            "assets/icons/high-beam-white.png", "assets/icons/light-black.png", "assets/icons/light-red.png",
-            "assets/icons/light-white.png", "assets/icons/motor-black.png", "assets/icons/motor-red.png",
-            "assets/icons/motor-white.png", "assets/icons/speed-black.png", "assets/icons/speed-red.png",
-            "assets/icons/speed-white.png"
-        ],
+        "leads_gui": ["assets/*", "assets/icons/*"],
         "leads_vec": ["_bootloader/leads_vec.service.sh"],
         "leads_vec_rc": ["_bootloader/leads_vec_rc.service.sh", "config.json"]
     },
