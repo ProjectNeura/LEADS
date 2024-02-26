e0f74405a40d446286962381ba741e14daa36741
ATATC <futerry@outlook.com>
2024-02-26 11:45:55 -0500

    Added leads_vec_power.ino.

---
+++ arduino/leads_vec_power/leads_vec_power.ino
@@ -0,0 +1,17 @@
+#include "LEADS.h"
+
+const int PIN_VOT[] = {2};
+
+void lfwssOnUpdate(float ws) {
+}
+
+VoltageSensor VOT(PIN_VOT);
+
+void setup() {
+    Serial.begin(9600);
+    VOT.initialize();
+}
+
+void loop() {
+    returnFloat("vot", VOT.read());
+}
