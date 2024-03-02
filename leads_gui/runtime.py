a3c8fdc421fc2b6a46e1ff3d005730a82eaadfdc
ATATC <futerry@outlook.com>
2024-03-01 21:29:45 -0500

    Supported dictionary type data in `RuntimeData.comm_notify()`.

--- leads_gui/runtime.py
+++ leads_gui/runtime.py
@@ -1,4 +1,6 @@
+from json import dumps as _dumps
 from time import time as _time
+from typing import Any as _Any
 
 from leads import DataContainer as _DataContainer
 from leads.comm import Server as _Server
@@ -9,9 +11,9 @@ class RuntimeData(object):
     lap_time: list[int] = []
     comm: _Server | None = None
 
-    def comm_notify(self, d: _DataContainer) -> None:
+    def comm_notify(self, d: _DataContainer | dict[str, _Any]) -> None:
         if self.comm:
-            self.comm.broadcast(d.encode())
+            self.comm.broadcast(d.encode() if isinstance(d, _DataContainer) else _dumps(d).encode())
 
     def comm_kill(self) -> None:
         if self.comm:
