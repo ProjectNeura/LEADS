54a594c7fccc16f96d89a2c1f4b1fba20fc33819
ATATC <futerry@outlook.com>
2024-03-02 23:20:14 -0500

    Replaced dependency `gps` with `pynmea2`.

--- leads_raspberry_pi/__init__.py
+++ leads_raspberry_pi/__init__.py
@@ -4,6 +4,8 @@ if not _find_spec("RPi"):
     raise ImportError("Please install `RPi.GPIO` to run this module\n>>>apt install python3-rpi.gpio")
 if not _find_spec("gps"):
     raise ImportError("Please install `gps` to run this module\n>>>pip install gps")
+if not _find_spec("pynmea2"):
+    raise ImportError("Please install `pynmea2` to run this module\n>>>pip install pynmea2")
 if not _find_spec("serial"):
     raise ImportError("Please install `pyserial` to run this module\n>>>pip install pyserial")
 
