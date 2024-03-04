483e60fc0111cb2d75dc3ff809b169340ce1b2c9
ATATC <futerry@outlook.com>
2024-03-04 13:45:18 -0500

    Added ecs mode overwriting.

--- leads/plugin/plugin.py
+++ leads/plugin/plugin.py
@@ -1,5 +1,6 @@
 from typing import Any as _Any
 
+from leads.constant import ECSMode
 from leads.context import Context, ContextAssociated
 from leads.types import RequiredData
 
@@ -12,13 +13,18 @@ class Plugin(ContextAssociated):
         super().__init__()
         self._required_data: RequiredData = required_data
         self.state: dict[str, _Any] = {}
-        self.enabled: bool = True
+        self._enabled: bool = True
 
     def __getitem__(self, key: str) -> _Any:
         return self.state[key]
 
     def __setitem__(self, key: str, value: _Any) -> None:
         self.state[key] = value
+
+    def enabled(self, enabled: bool | None = None) -> bool | None:
+        if enabled is None:
+            return self.require_context().ecs_mode() != ECSMode.OFF and self._enabled
+        self._enabled = enabled
 
     def required_data(self) -> list[str]:
         return (self._required_data if isinstance(self._required_data, list) else
