7a79c44c0bb21a4fb6bb85c474e55485b492dfb4
ATATC <futerry@outlook.com>
2024-03-12 12:55:55 -0400

    Added type `Number`.

--- leads/types.py
+++ leads/types.py
@@ -1,5 +1,6 @@
-from typing import Callable as _Callable
+from typing import Callable as _Callable, SupportsInt as _SupportsInt, SupportsFloat as _SupportsFloat
 
+type Number = int | float | _SupportsInt | _SupportsFloat
 type OptionalSpeed = float | None
 type Compressor[T] = _Callable[[list[T], int], list[T]]
 type Stringifier[T] = _Callable[[T], str]
