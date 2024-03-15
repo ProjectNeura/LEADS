95f73dcc31c14fd112c5a9695206f1dc0bb2baa0
ATATC <futerry@outlook.com>
2024-03-15 17:08:46 -0400

    Fixing #73.

--- leads_vec/_bootloader/leads_vec.service.sh
+++ leads_vec/_bootloader/leads_vec.service.sh
@@ -3,4 +3,5 @@ then
   echo "Error: Config file does not exist"
   exit 1
 fi
+/usr/bin/xhost +SI:localuser:"${USER}"
 python3.12 -m leads_vec -c /usr/local/leads/config.json run
--- leads_vec/_bootloader/systemd.py
+++ leads_vec/_bootloader/systemd.py
@@ -1,12 +1,10 @@
 from os import chmod as _chmod, getuid as _getuid
 from os.path import abspath as _abspath
 from pwd import getpwuid as _getpwuid
-from subprocess import run as _run
 
 
 def create_service() -> None:
     _chmod(script := _abspath(__file__)[:-10] + "leads_vec.service.sh", 777)
-    _run(("/usr/bin/xhost", "+SI:localuser:" + (user := _getpwuid(_getuid()).pw_name)))
     with open("/etc/systemd/system/leads_vec.service", "w") as f:
         f.write(
             "[Unit]\n"
@@ -15,7 +13,7 @@ def create_service() -> None:
             "Requires=display-manager.service\n"
             "[Service]\n"
             "Type=simple\n"
-            f"User={user}\n"
+            f"User={(user := _getpwuid(_getuid()).pw_name)}\n"
             "Environment=\"DISPLAY=:0\"\n"
             f"Environment=\"XAUTHORITY=/home/{user}/.Xauthority\"\n"
             f"ExecStart=/bin/bash {script}\n"
