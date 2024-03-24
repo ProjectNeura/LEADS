41073552b1a63e56049d2cf97b21bbd87099b902
ATATC <futerry@outlook.com>
2024-03-24 15:50:15 -0400

    Added LED blinking parameters.

--- leads_raspberry_pi/led.py
+++ leads_raspberry_pi/led.py
@@ -6,9 +6,11 @@ from leads import Device as _Device
 
 
 class LED(_Device):
-    def __init__(self, pin: int) -> None:
+    def __init__(self, pin: int, blink_time_on: float = 1, blink_time_off: float = 1) -> None:
         super().__init__(pin)
         self._led: _LED = _LED(pin)
+        self._blink_time_on: float = blink_time_on
+        self._blink_time_off: float = blink_time_off
 
     @_override
     def read(self) -> bool:
@@ -21,6 +23,6 @@ class LED(_Device):
         elif payload == 1:
             self._led.on()
         elif payload == 2:
-            self._led.blink()
+            self._led.blink(self._blink_time_on, self._blink_time_off)
         else:
             raise ValueError("Invalid payload: " + str(payload))
