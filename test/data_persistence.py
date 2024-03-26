36257e67a95d9289eb734951664feba81f4f1346
ATATC <futerry@outlook.com>
2024-03-26 10:33:07 -0400

    Removed tests.

--- test/data_persistence.py
+++
@@ -1,9 +0,0 @@
-from leads.data_persistence import DataPersistence
-
-fh = open("./test_data.csv", "a")
-dp = DataPersistence(fh, max_size=4)
-
-for i in range(1000):
-    dp.append(i)
-print(dp._chunk_size)
-print(dp)
