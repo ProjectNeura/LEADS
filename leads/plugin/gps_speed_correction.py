611ffc4391122c4c855269804b0647f081ac84de
ATATC <futerry@outlook.com>
2024-03-27 12:32:50 -0400

    Bug fixed: invalid syntax.

--- leads/plugin/gps_speed_correction.py
+++ leads/plugin/gps_speed_correction.py
@@ -16,7 +16,7 @@ class GPSSpeedCorrection(Plugin):
         d = context.data()
         if not d.gps_valid:
             return
-        self._loss += current_loss := d.speed - d.gps_ground_speed
+        self._loss += (current_loss := d.speed - d.gps_ground_speed)
         self._abs_loss += abs(current_loss)
         self._n += 1
         if d.speed < 5 and current_loss < self._abs_loss / self._n:
