018c830ea95ff3eccb5a6928fc464423d39f1ab2
ATATC <futerry@outlook.com>
2024-03-28 12:29:55 -0400

    Enabled all five wheel speed sensors.

--- arduino/leads_vec_wsc/leads_vec_wsc.ino
+++ arduino/leads_vec_wsc/leads_vec_wsc.ino
@@ -6,19 +6,25 @@ const int PIN_LRWSS[] = {4};
 const int PIN_RRWSS[] = {5};
 const int PIN_CRWSS[] = {6};
 
-void lfwssOnUpdate(float ws) {
-    returnFloat(LEFT_FRONT_WHEEL_SPEED_SENSOR, ws);
-    returnFloat(RIGHT_FRONT_WHEEL_SPEED_SENSOR, ws);
-    returnFloat(CENTER_REAR_WHEEL_SPEED_SENSOR, ws);
-}
-
-WheelSpeedSensor LFWSS(PIN_LFWSS, lfwssOnUpdate);
+WheelSpeedSensor LFWSS(PIN_LFWSS, [](float ws) {returnFloat(LEFT_FRONT_WHEEL_SPEED_SENSOR, ws);});
+WheelSpeedSensor RFWSS(PIN_RFWSS, [](float ws) {returnFloat(RIGHT_FRONT_WHEEL_SPEED_SENSOR, ws);});
+WheelSpeedSensor LRWSS(PIN_LRWSS, [](float ws) {returnFloat(LEFT_REAR_WHEEL_SPEED_SENSOR, ws);});
+WheelSpeedSensor RRWSS(PIN_RRWSS, [](float ws) {returnFloat(RIGHT_REAR_WHEEL_SPEED_SENSOR, ws);});
+WheelSpeedSensor CRWSS(PIN_CRWSS, [](float ws) {returnFloat(CENTER_REAR_WHEEL_SPEED_SENSOR, ws);});
 
 void setup() {
     Serial.begin(9600);
     LFWSS.initialize();
+    RFWSS.initialize();
+    LRWSS.initialize();
+    RRWSS.initialize();
+    CRWSS.initialize();
 }
 
 void loop() {
     LFWSS.read();
+    RFWSS.read();
+    LRWSS.read();
+    RRWSS.read();
+    CRWSS.read();
 }
