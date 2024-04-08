b7b28fb759beb844590672915a1b73bc0127bfb0
ATATC <futerry@outlook.com>
2024-04-08 14:24:00 -0400

    Bug fixed. (#71)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -18,7 +18,7 @@ execute_root() {
 }
 
 echo "Installing Python..."
-execute_root "wget" "-O" "get-pip.py" "https://raw.githubusercontent.com/ProjectNeura/LEADS/master/scripts/python-install.sh"
+execute_root "wget" "-O" "python-install.sh" "https://raw.githubusercontent.com/ProjectNeura/LEADS/master/scripts/python-install.sh"
 execute_root "/bin/sh" "python-install.sh"
 echo "Cleaning up..."
 execute_root "rm" "python-install.sh"
