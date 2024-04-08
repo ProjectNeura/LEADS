aa499af3de81a58ec590a18e54a12937bdf02034
ATATC <futerry@outlook.com>
2024-04-08 02:26:49 -0400

    Using `pynput` as the successor of `keyboard`. (#93)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,5 +5,5 @@ then
 fi
 sudo su
 ./python-install.sh
-python3.12 -m pip install Pillow customtkinter keyboard pyserial leads
+python3.12 -m pip install Pillow customtkinter pynput pyserial lgpio gpiozero pynmea2 leads
 python3.12 -m leads_vec info
