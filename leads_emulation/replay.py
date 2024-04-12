35b1893c51b1d5da04c626eb1c4cb6a668cee6d6
ATATC <futerry@outlook.com>
2024-04-11 21:21:13 -0400

    Completed replay controllers. (#95)

--- leads_emulation/replay.py
+++ leads_emulation/replay.py
@@ -1,41 +1,23 @@
-from importlib.util import find_spec as _find_spec
-
-if not _find_spec("pandas"):
-    raise ImportError("Please install `pandas` to run this module\n>>>pip install pandas")
-
-from typing import override as _override
+from typing import override as _override, Iterator as _Iterator, Any as _Any
 
 from leads import Controller as _Controller, SRWDataContainer as _SRWDataContainer, \
     DRWDataContainer as _DRWDataContainer
-
-
-class Dataset(object):
-    def __init__(self, data_dir: str, instruction_file: str) -> None:
-        self._data_dir: str = data_dir
-        self._instruction_file: str = instruction_file
-
-    def load(self) -> None:
-        # Todo
-        pass
+from leads.data_persistence import Dataset as _Dataset
 
 
 class ReplayController(_Controller):
-    def __init__(self, dataset: Dataset) -> None:
+    def __init__(self, dataset: _Dataset) -> None:
         super().__init__()
-        self._dataset: Dataset = dataset
-
-    @_override
-    def initialize(self, *parent_tags: str) -> None:
-        super().initialize(*parent_tags)
+        self._dataset: _Iterator[dict[str, _Any]] = iter(dataset)
 
 
 class SRWReplayController(ReplayController):
     @_override
     def read(self) -> _SRWDataContainer:
-        return _SRWDataContainer()
+        return _SRWDataContainer(**next(self._dataset))
 
 
 class DRWReplayController(ReplayController):
     @_override
     def read(self) -> _DRWDataContainer:
-        return _DRWDataContainer()
+        return _DRWDataContainer(**next(self._dataset))
