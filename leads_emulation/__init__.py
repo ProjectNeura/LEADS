52202445a719e4d288d8871a0f5ff94eb1e33105
ATATC <futerry@outlook.com>
2024-04-17 19:44:05 -0400

    Improved acceleration emulation.

--- leads_emulation/__init__.py
+++ leads_emulation/__init__.py
@@ -1,4 +1,5 @@
 from random import randint as _randint
+from time import time as _time
 from typing import override as _override
 
 from numpy import sin as _sin, pi as _pi
@@ -15,9 +16,19 @@ class _EmulatedController(_Controller):
         self.minimum: int = minimum
         self.maximum: int = maximum
         self.skid_possibility: float = skid_possibility
+        self._last_speed: float = 0
+        self._last_time: float = _time()
 
     def generate_rear_wheel_speed(self, front_wheel_speed: float) -> float:
         return front_wheel_speed + int(_randint(1, int(1 / self.skid_possibility)) * self.skid_possibility)
+
+    def generate_forward_acceleration(self, speed: float) -> float:
+        t = _time()
+        try:
+            return (speed - self._last_speed) / 3.6 / (t - self._last_time)
+        finally:
+            self._last_speed = speed
+            self._last_time = t
 
 
 class RandomController(_EmulatedController):
@@ -27,6 +38,7 @@ class RandomController(_EmulatedController):
                               speed=(fws := _randint(self.minimum, self.maximum)),
                               front_wheel_speed=fws,
                               rear_wheel_speed=self.generate_rear_wheel_speed(fws),
+                              forward_acceleration=self.generate_forward_acceleration(fws),
                               gps_valid=True,
                               gps_ground_speed=fws,
                               latitude=_randint(4315, 4415) / 100,
@@ -38,7 +50,7 @@ class SinController(_EmulatedController):
                  minimum: int = 30,
                  maximum: int = 40,
                  skid_possibility: float = .1,
-                 acceleration: float = .05) -> None:
+                 acceleration: float = .01) -> None:
         super().__init__(minimum, maximum, skid_possibility)
         self.acceleration: float = acceleration
         self.magnitude: int = int((maximum - minimum) * .5)
@@ -48,10 +60,10 @@ class SinController(_EmulatedController):
     def read(self) -> _DataContainer:
         try:
             return _DataContainer(voltage=48.0,
-                                  speed=(fws := ((p := _sin(self.counter)) * self.magnitude + self.magnitude)),
+                                  speed=(fws := (_sin(self.counter) * self.magnitude + self.magnitude)),
                                   front_wheel_speed=fws,
                                   rear_wheel_speed=self.generate_rear_wheel_speed(fws),
-                                  forward_acceleration=p,
+                                  forward_acceleration=self.generate_forward_acceleration(fws),
                                   gps_valid=True,
                                   gps_ground_speed=fws,
                                   latitude=_randint(4315, 4415) / 100,
