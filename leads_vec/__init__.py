e77a3324bc43e8505f7ed34d15b41a4afa31f070
ATATC <futerry@outlook.com>
2024-04-29 17:04:20 -0400

    Docs. (#117)

--- leads_vec/__init__.py
+++ leads_vec/__init__.py
@@ -2,3 +2,7 @@ from importlib.util import find_spec as _find_spec
 
 if not _find_spec("pynput"):
     raise ImportError("Please install `pynput` to run this module\n>>>pip install pynput")
+
+a = "customtkinter gpiozero lgpio Pillow pynput pyserial pynmea2 PySDL2".split()
+a.sort()
+print(" ".join(a))
