ccdf98811c065725e4f6e1db60af6343ae315289
ATATC <futerry@outlook.com>
2024-04-29 17:11:50 -0400

    Bug fixed.

--- leads_vec/__init__.py
+++ leads_vec/__init__.py
@@ -2,7 +2,3 @@ from importlib.util import find_spec as _find_spec
 
 if not _find_spec("pynput"):
     raise ImportError("Please install `pynput` to run this module\n>>>pip install pynput")
-
-a = "customtkinter gpiozero lgpio Pillow pynput pyserial pynmea2 PySDL2".split()
-a.sort()
-print(" ".join(a))
