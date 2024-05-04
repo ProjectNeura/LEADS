1f40773ae924ffe9a1d3cf7d14480358c7195c25
ATATC <futerry@outlook.com>
2024-05-04 16:34:31 -0400

    Supported checking for frp. (#124)

--- leads_vec/_bootloader/frp.py
+++ leads_vec/_bootloader/frp.py
@@ -1,5 +1,8 @@
+from os.path import exists as _exists
 from subprocess import run as _run
 
 
 def start_frpc() -> None:
+    if not _exists("/usr/local/frp/frpc") or not _exists("/usr/local/frp/frpc.toml"):
+        raise FileNotFoundError("frp not found")
     _run("/usr/local/frp/frpc -c /usr/local/frpc.toml")
