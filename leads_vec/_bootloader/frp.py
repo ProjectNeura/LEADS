4900d17b3cca535d1d4b0e9230b98ff626c8587c
ATATC <futerry@outlook.com>
2024-05-05 12:16:40 -0400

    Bug fixed: frpc blocks the main thread. (https://github.com/ProjectNeura/LEADS/issues/124#issuecomment-2094863679)

--- leads_vec/_bootloader/frp.py
+++ leads_vec/_bootloader/frp.py
@@ -1,8 +1,10 @@
 from os.path import exists as _exists
 from subprocess import run as _run
+from threading import Thread as _Thread
 
 
 def start_frpc() -> None:
     if not _exists("/usr/local/frp/frpc") or not _exists("/usr/local/frp/frpc.toml"):
         raise FileNotFoundError("frp not found")
-    _run(("/usr/local/frp/frpc", "-c", "/usr/local/frp/frpc.toml"))
+    _Thread(name="frpc", target=_run, args=(("/usr/local/frp/frpc", "-c", "/usr/local/frp/frpc.toml"),),
+            daemon=True).start()
