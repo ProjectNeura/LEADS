c41305ddf6f2601385ea870b0eff27a0d560e003
ATATC <futerry@outlook.com>
2024-05-05 12:25:22 -0400

    Supported silent subprocess and logging when it unexpectedly ends. (https://github.com/ProjectNeura/LEADS/issues/124#issuecomment-2094867522)

--- leads_vec/_bootloader/frp.py
+++ leads_vec/_bootloader/frp.py
@@ -1,10 +1,16 @@
 from os.path import exists as _exists
-from subprocess import run as _run
+from subprocess import run as _run, DEVNULL as _DEVNULL
 from threading import Thread as _Thread
+
+from leads import L as _L
 
 
 def start_frpc() -> None:
     if not _exists("/usr/local/frp/frpc") or not _exists("/usr/local/frp/frpc.toml"):
         raise FileNotFoundError("frp not found")
-    _Thread(name="frpc", target=_run, args=(("/usr/local/frp/frpc", "-c", "/usr/local/frp/frpc.toml"),),
-            daemon=True).start()
+
+    def wrapper() -> None:
+        _run(("/usr/local/frp/frpc", "-c", "/usr/local/frp/frpc.toml"), stdout=_DEVNULL, stderr=_DEVNULL)
+        _L.error("frpc exits prematurely")
+
+    _Thread(name="frpc", target=wrapper, daemon=True).start()
