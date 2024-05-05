d7985d64be789176c79883ad6663c96b707e804d
ATATC <futerry@outlook.com>
2024-05-05 12:07:21 -0400

    Bug fixed. (#124)

--- leads_vec/_bootloader/frp.py
+++ leads_vec/_bootloader/frp.py
@@ -5,4 +5,4 @@ from subprocess import run as _run
 def start_frpc() -> None:
     if not _exists("/usr/local/frp/frpc") or not _exists("/usr/local/frp/frpc.toml"):
         raise FileNotFoundError("frp not found")
-    _run(("/usr/local/frp/frpc", "-c", "/usr/local/frpc.toml"))
+    _run(("/usr/local/frp/frpc", "-c", "/usr/local/frp/frpc.toml"))
