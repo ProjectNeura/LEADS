5dfe15705f3cb2a5fbf5ae38cc3706681ce186d5
Terry Fu <futerry@outlook.com>
2024-05-05 12:51:13 -0400

    Added `pysdl2-dll` to the optional dependencies. (#125) (#128)

--- leads_audio/__init__.py
+++ leads_audio/__init__.py
@@ -1,6 +1,6 @@
 from importlib.util import find_spec as _find_spec
 
 if not _find_spec("sdl2"):
-    raise ImportError("Please install `PySDL2` to run this module\n>>>pip install PySDL2")
+    raise ImportError("Please install `PySDL2` and `pysdl2-dll` to run this module\n>>>pip install PySDL2 pysdl2-dll")
 
 from leads_audio.prototype import *
