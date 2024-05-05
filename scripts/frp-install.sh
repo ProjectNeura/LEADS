00cc601f8ad363b12a67975d3f0e4e32bafba079
ATATC <futerry@outlook.com>
2024-05-05 11:58:22 -0400

    Supported different kernels and architectures. (https://github.com/ProjectNeura/LEADS/issues/124#issuecomment-2094377028)

--- scripts/frp-install.sh
+++ scripts/frp-install.sh
@@ -27,15 +27,16 @@ then
   echo "cURL is not available, installing..."
   execute_root "apt" "install" "-y" "curl"
 fi
-latest_release=$(curl -s "https://api.github.com/repos/fatedier/frp/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")' | cut -c 2-)
+latest_release=$(curl -s "https://api.github.com/repos/fatedier/frp/releases/latest" | grep -o '"tag_name": "[^"]*' | grep -o '[^"]*$' | cut -c 2-)
 if [ -z "$latest_release" ];
 then abort "Failed to retrieve the latest release"
 fi
-echo "Downloading frp@${latest_release}..."
-execute_root "wget" "-O" "frp.tar.gz" "https://github.com/fatedier/frp/releases/download/v${latest_release}/frp_${latest_release}_linux_amd64.tar.gz"
+filename="frp_${latest_release}_$(uname -s | tr '[:upper:]' '[:lower:]')_$(uname -m)"
+echo "Downloading ${filename}.tar.gz..."
+execute_root "wget" "-O" "frp.tar.gz" "https://github.com/fatedier/frp/releases/download/v${latest_release}/${filename}.tar.gz"
 echo "Extracting..."
 execute_root "tar" "-xzvf" "frp.tar.gz"
-echo "Moving frp_${latest_release}_linux_amd64 to /usr/local/frp..."
-execute_root "mv" "frp_${latest_release}_linux_amd64" "/usr/local/frp"
+echo "Moving ${filename} to /usr/local/frp..."
+execute_root "mv" "${filename}" "/usr/local/frp"
 echo "Cleaning up..."
 execute_root "rm" "frp.tar.gz"
