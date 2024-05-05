5fad5c1372420dc405b13e9979f1b8a39a463ad5
ATATC <futerry@outlook.com>
2024-05-05 12:31:02 -0400

    Error messages improved. (#124)

--- leads_vec/_bootloader/frp.py
+++ leads_vec/_bootloader/frp.py
@@ -1,5 +1,5 @@
 from os.path import exists as _exists
-from subprocess import run as _run, DEVNULL as _DEVNULL
+from subprocess import run as _run, PIPE as _PIPE
 from threading import Thread as _Thread
 
 from leads import L as _L
@@ -10,7 +10,8 @@ def start_frpc() -> None:
         raise FileNotFoundError("frp not found")
 
     def wrapper() -> None:
-        _run(("/usr/local/frp/frpc", "-c", "/usr/local/frp/frpc.toml"), stdout=_DEVNULL, stderr=_DEVNULL)
-        _L.error("frpc exits prematurely")
+        r = _run(("/usr/local/frp/frpc", "-c", "/usr/local/frp/frpc.toml"), stdout=_PIPE, stderr=_PIPE)
+        _L.error("`frpc` exits prematurely")
+        _L.debug(f"Console output:\n{r.stdout.decode()}")
 
     _Thread(name="frpc", target=wrapper, daemon=True).start()
