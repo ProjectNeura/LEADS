1ee1ab0b1ad8c56319c3b3e377c8405031670e0b
ATATC <futerry@outlook.com>
2024-05-06 09:59:18 -0400

    Improved compatibility.

--- leads_vec/_bootloader/frp.py
+++ leads_vec/_bootloader/frp.py
@@ -3,14 +3,17 @@ from subprocess import run as _run, PIPE as _PIPE
 from threading import Thread as _Thread
 
 from leads import L as _L
+from leads_gui.system import get_system_kernel as _get_system_kernel
 
 
 def start_frpc() -> None:
-    if not _exists("/usr/local/frp/frpc") or not _exists("/usr/local/frp/frpc.toml"):
+    path = "%AppData%/frp" if _get_system_kernel() == "windows" else "/usr/local/frp"
+
+    if not _exists(f"{path}/frpc") or not _exists(f"{path}/frpc.toml"):
         raise FileNotFoundError("frp not found")
 
     def wrapper() -> None:
-        r = _run(("/usr/local/frp/frpc", "-c", "/usr/local/frp/frpc.toml"), stdout=_PIPE, stderr=_PIPE)
+        r = _run((f"{path}/frpc", "-c", f"{path}/frpc.toml"), stdout=_PIPE, stderr=_PIPE)
         _L.error("`frpc` exits prematurely")
         _L.debug(f"Console output:\n{r.stdout.decode()}")
 
