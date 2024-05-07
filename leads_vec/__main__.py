d09b9849410cfd1c9c5a5933be28a88c9542c5ed
Terry Fu <futerry@outlook.com>
2024-05-06 21:55:01 -0400

    Improve `leads_vec` Debugging Information (#132)
    * Added `auto_path()` and `frpc_exists()`.
    
    * Included module path and coworker availability. (#131)
    
    * Improved debugging info. (#131)

--- leads_vec/__main__.py
+++ leads_vec/__main__.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser as _ArgumentParser, BooleanOptionalAction as _BooleanOptionalAction
+from importlib.metadata import version as _package_version, PackageNotFoundError as _PackageNotFoundError
 from importlib.util import spec_from_file_location as _spec_from_file_location, module_from_spec as _module_from_spec
 from os import mkdir as _mkdir, chmod as _chmod
 from os.path import abspath as _abspath
@@ -17,6 +18,7 @@ from leads_gui.system import get_system_kernel as _get_system_platform
 
 if __name__ == "__main__":
     _filterwarnings("ignore")
+    _MODULE_PATH = _abspath(__file__)[:-12]
 
     parser = _ArgumentParser(prog="LEADS VeC",
                              description="Lightweight Embedded Assisted Driving System VeC",
@@ -24,7 +26,7 @@ if __name__ == "__main__":
                                     "GitHub: https://github.com/ProjectNeura/LEADS")
     parser.add_argument("action", choices=("info", "replay", "run"))
     parser.add_argument("-c", "--config", default=None, help="specify a configuration file")
-    parser.add_argument("-d", "--devices", default=f"{_abspath(__file__)[:-11]}devices.py",
+    parser.add_argument("-d", "--devices", default=f"{_MODULE_PATH}/devices.py",
                         help="specify a devices module")
     parser.add_argument("-r", "--register", choices=("systemd", "config", "reverse_proxy"), default=None,
                         help="register a service")
@@ -39,10 +41,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.action == "info":
         from leads_vec.__version__ import __version__
+        from ._bootloader import frpc_exists as _frpc_exists
 
-        _L.info(f"LEADS Version: {__version__}",
+        leads_version = "Unknown"
+        try:
+            leads_version = _package_version("leads")
+        except _PackageNotFoundError:
+            _L.warn("Failed to retrieve package version (did you install through pip?)")
+        _L.info(f"LEADS VeC",
                 f"System Platform: {_get_system_platform()}",
                 f"Python Version: {_version}",
+                f"`frpc` Available: {_frpc_exists()}",
+                f"Module Path: {_MODULE_PATH}",
+                f"LEADS Version: {leads_version}",
+                f"LEADS VeC Version: {__version__}",
                 sep="\n")
         _exit()
     if args.register == "systemd":
