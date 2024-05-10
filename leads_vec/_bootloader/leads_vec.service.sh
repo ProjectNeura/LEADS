bef8c7f45a6a9583fdd32ce2708ad93422cb3ede
ATATC <futerry@outlook.com>
2024-05-10 17:06:47 -0400

    Added xHost check. (#151)

--- leads_vec/_bootloader/leads_vec.service.sh
+++ leads_vec/_bootloader/leads_vec.service.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
 if [ ! -x "/usr/local/leads/config.json" ];
 then
   echo "Error: Config file does not exist"
   exit 1
 fi
-
+while ! xhost >& /dev/null;
+do sleep 1
+done
 # change the interpreter or adjust the arguments according to your needs
 # do not remove `--xws`
 python-leads -m leads_vec -c /usr/local/leads/config.json --xws run
