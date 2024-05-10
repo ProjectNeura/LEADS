7df28395f1c3ce71174a8f001b8229dae16d465f
ATATC <futerry@outlook.com>
2024-05-09 21:07:37 -0400

    Prompts improved.

--- leads_vec/_bootloader/leads_vec.service.sh
+++ leads_vec/_bootloader/leads_vec.service.sh
@@ -3,4 +3,7 @@ then
   echo "Error: Config file does not exist"
   exit 1
 fi
+
+# change the interpreter or adjust the arguments according to your needs
+# but do not remove `--xws`
 /usr/local/leads/venv/bin/python -m leads_vec -c /usr/local/leads/config.json --xws run
