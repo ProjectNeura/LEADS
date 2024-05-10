59b82eb071cfb35e0e2a3e2890d7ea06ab3e98e3
ATATC <futerry@outlook.com>
2024-05-09 20:16:33 -0400

    Added virtual environment support. (#142)

--- leads_vec/_bootloader/leads_vec.service.sh
+++ leads_vec/_bootloader/leads_vec.service.sh
@@ -3,4 +3,4 @@ then
   echo "Error: Config file does not exist"
   exit 1
 fi
-python3.12 -m leads_vec -c /usr/local/leads/config.json --xws run
+/usr/local/leads/venv/bin/python -m leads_vec -c /usr/local/leads/config.json --xws run
