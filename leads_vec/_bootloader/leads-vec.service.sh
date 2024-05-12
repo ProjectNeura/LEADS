899d2369467b4a6d28348e860cd93a5aea29a900
ATATC <futerry@outlook.com>
2024-05-12 12:29:30 -0400

    Prompts improved.

--- leads_vec/_bootloader/leads-vec.service.sh
+++ leads_vec/_bootloader/leads-vec.service.sh
@@ -2,7 +2,7 @@
 
 if test ! -r "/usr/local/leads/config.json"
 then
-  echo "Error: Config file does not exist"
+  echo "Error: Config file does not exist or not readable"
   exit 1
 fi
 # change the interpreter or adjust the arguments according to your needs
