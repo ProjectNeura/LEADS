2d003625490d1fa3d4de9ef844256030a15fd5d5
ATATC <futerry@outlook.com>
2024-05-12 14:19:34 -0400

    Added `run()`. (#162)

--- leads_vec/__init__.py
+++ leads_vec/__init__.py
@@ -2,3 +2,5 @@ from importlib.util import find_spec as _find_spec
 
 if not _find_spec("pynput"):
     raise ImportError("Please install `pynput` to run this module\n>>>pip install pynput")
+
+from leads_vec.run import *
---
+++ leads_vec/run.py
@@ -0,0 +1,13 @@
+from typing import Literal as _Literal
+
+
+def run(config: str,
+        devices: str,
+        register: _Literal["systemd", "config", "reverse_proxy"],
+        theme: str,
+        magnify_font_sizes: float,
+        emu: bool,
+        auto_mfs: bool,
+        ignore_import_error: bool) -> None:
+    # TODO
+    raise NotImplementedError
