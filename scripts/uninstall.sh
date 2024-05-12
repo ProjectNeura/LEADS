9b1ad568e240aeba35fbd414b3d15e72fea21732
ATATC <futerry@outlook.com>
2024-05-12 13:24:26 -0400

    Added a Script for Uninstalling. (#161)

---
+++ scripts/uninstall.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+abort() {
+  printf "%s\n" "$@" >&2
+  exit 1
+}
+
+if [ "${EUID:-$(id -u)}" -ne 0 ]
+then abort "Error: This script requires root permission"
+fi
+
+execute() {
+  if ! "$@"
+  then abort "$(printf "Failed: %s" "$@")"
+  fi
+}
+
+execute_root() {
+  execute "sudo" "$@"
+}
+
+ask() {
+  printf "%s >>>" "$@" >&0
+  read -r input
+  echo "$input" | tr "[:upper:]" "[:lower:]"
+}
+
+if systemctl --user status "leads-vec" &> /dev/null
+then
+  echo "Disabling Systemd service leads-vec..."
+  execute_root "systemctl" "--user" "disable" "leads-vec"
+  echo "Removing Systemd service leads-vec..."
+  execute_root "rm" "/home/$(logname)/.config/systemd/user/leads-vec.service"
+  echo
+else echo "Systemd service leads-vec not detected, skipping..."
+fi
+
+if test -e "/usr/local/frp"
+then
+  if test "$(ask "Do you want to remove FRP? (Y/n)")" = "y"
+  then
+    echo "Removing FRP..."
+    execute_root "rm" "-r" "/usr/local/frp"
+  else echo "Abort"
+  fi
+else echo "FRP not detected, skipping..."
+fi
+
+if test -e "/usr/local/leads/config.json"
+then
+  if test "$(ask "Do you want to remove the configuration file? (Y/n)")" = "y"
+  then execute_root "rm" "/usr/local/leads/config.json"
+  else echo "Abort"
+  fi
+else echo "Configuration file not detected, skipping..."
+fi
+
+if test -e "/bin/pip-leads"
+then
+  echo "Removing soft link /bin/pip-leads..."
+  execute_root "rm" "/bin/pip-leads"
+else echo "Soft link /bin/pip-leads not detected, skipping..."
+fi
+
+if test -e "/bin/python-leads"
+then
+  echo "Removing soft link /bin/python-leads..."
+  execute_root "rm" "/bin/python-leads"
+else echo "Soft link /bin/python-leads not detected, skipping..."
+fi
+
+if test -e "/usr/local/leads/venv"
+then
+  echo "Removing virtual environment"
+  execute_root "rm" "-r" "/usr/local/leads/venv"
+else echo "Virtual environment not detected, skipping..."
+fi
+
+echo "LEADS has been uninstalled"
