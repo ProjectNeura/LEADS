7fe2c26242b40d0546d30a7069621ce46f6ea4c5
ATATC <futerry@outlook.com>
2024-05-14 18:34:51 -0400

    Bug fixed: unused imports. (#173)

--- leads/data_persistence/computational/gpu.py
+++ leads/data_persistence/computational/gpu.py
@@ -1,1 +1,11 @@
+import cupy as _cupy
 
+mean: type[_cupy.mean] = _cupy.mean
+array: type[_cupy.array] = _cupy.array
+norm: type[_cupy.linalg.norm] = _cupy.linalg.norm
+
+import pandas as _cudf
+
+read_csv: type[_cudf.read_csv] = _cudf.read_csv
+DataFrame: type[_cudf.DataFrame] = _cudf.DataFrame
+TextFileReader: type[_cudf.io.parsers.TextFileReader] = _cudf.io.parsers.TextFileReader
--- leads/data_persistence/computational/stable.py
+++ leads/data_persistence/computational/stable.py
@@ -1,1 +1,11 @@
+import numpy as _numpy
 
+mean: type[_numpy.mean] = _numpy.mean
+array: type[_numpy.array] = _numpy.array
+norm: type[_numpy.linalg.norm] = _numpy.linalg.norm
+
+import pandas as _pandas
+
+read_csv: type[_pandas.read_csv] = _pandas.read_csv
+DataFrame: type[_pandas.DataFrame] = _pandas.DataFrame
+TextFileReader: type[_pandas.io.parsers.TextFileReader] = _pandas.io.parsers.TextFileReader
