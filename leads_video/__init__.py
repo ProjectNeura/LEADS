b10b6d6eca36187b078b7d160fd96fa6cf671e41
ATATC <futerry@outlook.com>
2024-06-28 21:09:37 +0800

    Added camera support. (#222)

--- leads_video/__init__.py
+++ leads_video/__init__.py
@@ -3,4 +3,4 @@ from importlib.util import find_spec as _find_spec
 if not _find_spec("cv2"):
     raise ImportError("Please install `opencv-python` to run this module\n>>>pip install opencv-python")
 
-from leads_video import *
+from leads_video.camera import *
