38d003161a87f0785de334d197a716a47d97d20a
Terry Fu <futerry@outlook.com>
2024-06-29 22:06:41 +0800

    Include Front View in the Data Container (#227)
    * Supported Base64 encoding. (#225)
    
    * Supported specifying camera's output resolution. (#222) (#225)
    
    * Bug fixed. (#222) (#225)
    
    * Bug fixed. (#222) (#225)
    
    * Added visual data container. (#225)

--- leads_video/__init__.py
+++ leads_video/__init__.py
@@ -2,5 +2,8 @@ from importlib.util import find_spec as _find_spec
 
 if not _find_spec("cv2"):
     raise ImportError("Please install `opencv-python` to run this module\n>>>pip install opencv-python")
+if not _find_spec("PIL"):
+    raise ImportError("Please install `Pillow` to run this module\n>>>pip install Pillow")
 
 from leads_video.camera import *
+from leads_video.utils import *
--- leads_video/camera.py
+++ leads_video/camera.py
@@ -1,7 +1,8 @@
 from typing import override as _override
 
+from PIL.Image import fromarray as _fromarray
 from cv2 import VideoCapture as _VideoCapture, cvtColor as _cvtColor, COLOR_BGR2RGB as _COLOR_BGR2RGB
-from numpy import ndarray as _ndarray
+from numpy import ndarray as _ndarray, transpose as _transpose, pad as _pad, array as _array
 
 from leads import Device as _Device
 
@@ -19,12 +20,30 @@ class Camera(_Device):
 
     @_override
     def write(self, payload: tuple[int, int] | None) -> None:
+        """
+        :param payload: [width, height]
+        """
         self._resolution = payload
+
+    def transform(self, x: _ndarray) -> _ndarray:
+        height, width = x.shape[:-1]
+        target_ratio = self._resolution[1] / self._resolution[0]
+        target_height = int(target_ratio * width)
+        pad_left, pad_right, pad_top, pad_bottom = 0, 0, 0, 0
+        if height > target_height:
+            target_width = int(height / target_ratio)
+            pad_left = (target_width - width) // 2
+            pad_right = target_width - width - pad_left
+        else:
+            pad_top = (target_height - height) // 2
+            pad_bottom = target_height - height - pad_top
+        x = _pad(x, ((0, 0), (pad_top, pad_bottom), (pad_left, pad_right)))
+        return _array(_fromarray(x).resize(self._resolution))
 
     @_override
     def read(self) -> _ndarray:
         _, frame = self._video_capture.read()
-        return _cvtColor(frame, _COLOR_BGR2RGB)
+        return _transpose(_cvtColor(self.transform(frame) if self._resolution else frame, _COLOR_BGR2RGB), (2, 0, 1))
 
     @_override
     def close(self) -> None:
