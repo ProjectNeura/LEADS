cb3ede5f499a7251ffed66da14d41ce008515604
Terry Fu <futerry@outlook.com>
2024-06-30 23:06:24 +0800

    Fixed direction indicator logic. (#234) (#237)

--- leads/context.py
+++ leads/context.py
@@ -85,15 +85,21 @@ class Context(_Generic[T], metaclass=_ABCMeta):
         return float(_average(_diff(_array(self._speed_seq)))) if len(self._speed_seq) > 1 else 0
 
     def left_indicator(self, left_indicator: bool | None = None, override: bool = False) -> bool | None:
-        if self._hazard and not override:
-            return True
+        if not override:
+            if self._hazard:
+                return True
+            if left_indicator:
+                self.right_indicator(False, True)
         if left_indicator is None:
             return self._left_indicator
         self._left_indicator = left_indicator
 
     def right_indicator(self, right_indicator: bool | None = None, override: bool = False) -> bool | None:
-        if self._hazard and not override:
-            return True
+        if not override:
+            if self._hazard:
+                return True
+            if right_indicator:
+                self.left_indicator(False, True)
         if right_indicator is None:
             return self._right_indicator
         self._right_indicator = right_indicator
