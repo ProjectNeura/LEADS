419bdcb6e3f6c39704ae620796544c86f850a2a2
ATATC <futerry@outlook.com>
2024-07-01 13:11:29 +0800

    Supported adaptive camera configuration. (#231)

--- leads_vec/devices_jarvis.py
+++ leads_vec/devices_jarvis.py
@@ -1,7 +1,8 @@
 from typing import override
 
 from leads import device, MAIN_CONTROLLER, mark_device, FRONT_VIEW_CAMERA, LEFT_VIEW_CAMERA, RIGHT_VIEW_CAMERA, \
-    REAR_VIEW_CAMERA
+    REAR_VIEW_CAMERA, require_config
+from leads_gui import Config
 from leads_video import Camera, base64_encode
 
 import_error: ImportError | None = None
@@ -10,10 +11,25 @@ try:
 except ImportError as e:
     import_error = e
 
+config: Config = require_config()
+CAMERA_RESOLUTION: tuple[int, int] | None = (480, 360)
+CAMERA_TAGS: list[str] = []
+CAMERA_ARGS: list[tuple[int, tuple[int, int] | None]] = []
+if (port := config.get("front_view_camera_port")) is not None:
+    CAMERA_TAGS.append(FRONT_VIEW_CAMERA)
+    CAMERA_ARGS.append((port, CAMERA_RESOLUTION))
+if (port := config.get("left_view_camera_port")) is not None:
+    CAMERA_TAGS.append(LEFT_VIEW_CAMERA)
+    CAMERA_ARGS.append((port, CAMERA_RESOLUTION))
+if (port := config.get("right_view_camera_port")) is not None:
+    CAMERA_TAGS.append(RIGHT_VIEW_CAMERA)
+    CAMERA_ARGS.append((port, CAMERA_RESOLUTION))
+if (port := config.get("rear_view_camera_port")) is not None:
+    CAMERA_TAGS.append(REAR_VIEW_CAMERA)
+    CAMERA_ARGS.append((port, CAMERA_RESOLUTION))
 
-@device((FRONT_VIEW_CAMERA, LEFT_VIEW_CAMERA, RIGHT_VIEW_CAMERA, REAR_VIEW_CAMERA), MAIN_CONTROLLER, [
-    (0, (480, 360)), (1, (480, 360)), (2, (480, 360)), (3, (480, 360))
-])
+
+@device(CAMERA_TAGS, MAIN_CONTROLLER, CAMERA_ARGS)
 class Cameras(Camera):
     @override
     def initialize(self, *parent_tags: str) -> None:
