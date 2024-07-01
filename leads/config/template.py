0e360f6ee4904f923b40b4921341dbdacad2be75
ATATC <futerry@outlook.com>
2024-07-01 18:28:44 +0800

    Restricted the types of attributes in `Config`. (#238)

--- leads/config/template.py
+++ leads/config/template.py
@@ -1,17 +1,18 @@
 from json import dumps as _dumps
-from typing import Any as _Any, override as _override, Literal as _Literal
+from typing import override as _override, Literal as _Literal, Any as _Any
 
 from leads.data import Serializable
+from leads.types import SupportedConfig as _SupportedConfig
 
 
 class ConfigTemplate(Serializable):
-    def __init__(self, base: dict[str, _Any]) -> None:
+    def __init__(self, base: dict[str, _SupportedConfig]) -> None:
         """
         All custom attributes should be public (not named after "_").
         Writable attributes should start with "w_" such as "w_debug_level".
         :param base: the base dictionary
         """
-        self._d: dict[str, _Any] = base
+        self._d: dict[str, _SupportedConfig] = self.fix_dict(base)
         self._frozen: bool = False
         self.w_debug_level: _Literal["DEBUG", "INFO", "WARN", "ERROR"] = "DEBUG"
         self.data_seq_size: int = 100
@@ -19,16 +20,26 @@ class ConfigTemplate(Serializable):
         self.data_dir: str = "data"
         self.refresh()
 
-    def __getitem__(self, name: str) -> _Any | None:
+    def fix_dict(self, d: dict[str, _Any]) -> dict[str, _SupportedConfig]:
+        return {k: self.fix_type(v) for k, v in d.items()}
+
+    def fix_type(self, value: _Any) -> _SupportedConfig:
+        if isinstance(value, (tuple, list)):
+            return tuple(self.fix_type(i) for i in value)
+        if not isinstance(value, (bool, int, float, str, type(None))):
+            raise TypeError(f"Unsupported value type: {value}")
+        return value
+
+    def __getitem__(self, name: str) -> _SupportedConfig | None:
         return self.get(name)
 
-    def __setitem__(self, name: str, value: _Any) -> None:
+    def __setitem__(self, name: str, value: _SupportedConfig) -> None:
         self.set(name, value)
 
     @_override
-    def __setattr__(self, name: str, value: _Any) -> None:
+    def __setattr__(self, name: str, value: _SupportedConfig) -> None:
         if self._writable(name):
-            super().__setattr__(name, value)
+            super().__setattr__(name, value if name.startswith("_") else self.fix_type(value))
 
     @_override
     def __str__(self) -> str:
@@ -39,35 +50,27 @@ class ConfigTemplate(Serializable):
         return _dumps(self.to_dict())
 
     @_override
-    def to_dict(self) -> dict[str, _Any]:
+    def to_dict(self) -> dict[str, _SupportedConfig]:
         return self._d.copy()
-
-    def load(self, d: dict[str, _Any]) -> None:
-        """
-        Load a new dictionary and refresh.
-        :param d: the dictionary to load
-        """
-        self._d = d
-        self.refresh()
 
     def _writable(self, name: str) -> bool:
         """
         :param name: the configuration name
         :return: True: writable; False: readonly
         """
-        return not hasattr(self, "_frozen") or not self._frozen or name.startswith("_") or name.startswith("w_")
+        return not hasattr(self, "_frozen") or not self._frozen or name.startswith("w_")
 
-    def set(self, name: str, value: _Any) -> None:
+    def set(self, name: str, value: _SupportedConfig) -> None:
         """
         Set the value with the given name in the dictionary.
         :param name: the dictionary key
         :param value: the value to set
         """
         if self._writable(name):
-            self._d[name] = value
+            self._d[name] = self.fix_type(value)
             self.refresh()
 
-    def get(self, name: str, default: _Any | None = None) -> _Any | None:
+    def get(self, name: str, default: _SupportedConfig | None = None) -> _SupportedConfig | None:
         """
         Get the value of a given name from the dictionary.
         :param name: the dictionary key
