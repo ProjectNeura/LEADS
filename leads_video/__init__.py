07c369a924632a36e4a47cda566364f53ef3ccb0
Terry Fu <futerry@outlook.com>
2024-07-03 19:13:44 +0800

    Changed the OpenCV dependency from `opencv-python` to `opencv-python-headless`. (#257) (#258)

--- leads_video/__init__.py
+++ leads_video/__init__.py
@@ -1,7 +1,7 @@
 from importlib.util import find_spec as _find_spec
 
 if not _find_spec("cv2"):
-    raise ImportError("Please install `opencv-python` to run this module\n>>>pip install opencv-python")
+    raise ImportError("Please install `opencv-python-headless` to run this module\n>>>pip install opencv-python")
 if not _find_spec("PIL"):
     raise ImportError("Please install `Pillow` to run this module\n>>>pip install Pillow")
 
