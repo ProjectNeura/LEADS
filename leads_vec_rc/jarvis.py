a34b7b1daf256ba350cd79a51b08acd1162c2a05
ATATC <futerry@outlook.com>
2024-07-07 15:16:06 +0800

    Added accepted class person. (#271)

--- leads_vec_rc/jarvis.py
+++ leads_vec_rc/jarvis.py
@@ -11,5 +11,5 @@ def process(d: dict[str, _Any]) -> dict[str, _Any]:
     for key in "front_view_base64", "left_view_base64", "right_view_base64", "rear_view_base64":
         if key in d.keys():
             img = base64_decode(d[key])
-            d[key] = "" if img is None else base64_encode(_model.mark(img, filter_type=("car",)))
+            d[key] = "" if img is None else base64_encode(_model.mark(img, None, ("car", "person")))
     return d
