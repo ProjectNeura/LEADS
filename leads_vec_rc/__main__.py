02739974ce1396ab566fb09d514d1f9c873846a9
ATATC <futerry@outlook.com>
2024-07-14 20:50:57 +0800

    Code reformatted.

--- leads_vec_rc/__main__.py
+++ leads_vec_rc/__main__.py
@@ -4,6 +4,7 @@ from uvicorn import run as _run
 
 from leads import register_config as _register_config, load_config as _load_config
 from leads_gui import Config as _Config
+from leads_vec_rc.cli import app
 
 if __name__ == "__main__":
     parser = _ArgumentParser(prog="LEADS VeC RC",
@@ -13,6 +14,4 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--port", type=int, default=8000, help="specify a server port")
     args = parser.parse_args()
     _register_config(_load_config(args.config, _Config) if args.config else _Config({}))
-    from leads_vec_rc.cli import app
-
     _run(app, host="0.0.0.0", port=args.port, log_level="warning")
