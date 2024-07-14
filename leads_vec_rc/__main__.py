0cbd31022977a2715a28dbf8ae172857d0dcaf1f
ATATC <futerry@outlook.com>
2024-07-14 20:45:16 +0800

    Hints improved.

--- leads_vec_rc/__main__.py
+++ leads_vec_rc/__main__.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     parser = _ArgumentParser(prog="LEADS VeC RC",
                              description="Lightweight Embedded Assisted Driving System VeC Remote Controller",
                              epilog="GitHub: https://github.com/ProjectNeura/LEADS")
-    parser.add_argument("-c", "--config", default=None, help="specified configuration file")
-    parser.add_argument("-p", "--port", type=int, default=8000, help="server port")
+    parser.add_argument("-c", "--config", default=None, help="specify a configuration file")
+    parser.add_argument("-p", "--port", type=int, default=8000, help="specify a server port")
     args = parser.parse_args()
     _register_config(_load_config(args.config, _Config) if args.config else _Config({}))
     from leads_vec_rc.cli import app
