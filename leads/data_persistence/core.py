141570de91361451a894f86a3783b5bcbb0711fd
ATATC <futerry@outlook.com>
2024-07-24 22:17:34 +0800

    Bug fixed: wrong header. (#305) (#308)

--- leads/data_persistence/core.py
+++ leads/data_persistence/core.py
@@ -261,7 +261,7 @@ DEFAULT_HEADER_FULL: tuple[str, str, str, str, str, str, str, str, str, str, str
 )
 VISUAL_HEADER_ONLY: tuple[str, str, str, str, str, str, str, str] = (
     "front_view_base64", "front_view_latency", "left_view_base64", "left_view_latency", "right_view_base64",
-    "front_view_latency", "rear_view_base64", "rear_view_latency"
+    "right_view_latency", "rear_view_base64", "rear_view_latency"
 )
 VISUAL_HEADER: _VisualHeader = DEFAULT_HEADER + VISUAL_HEADER_ONLY
 VISUAL_HEADER_FULL: _VisualHeaderFull = DEFAULT_HEADER_FULL + VISUAL_HEADER_ONLY
