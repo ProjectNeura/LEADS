8cb887b485d618f74b5745487f60693f232206f1
Hayden <Hhour@villanovacollege.ca>
2024-07-26 18:03:38 -0400

    #206 fixed variables to more percise values

--- arduino/leads_vec_power/Pedal.h
+++ arduino/leads_vec_power/Pedal.h
@@ -1,6 +1,6 @@
 const int hallSensorPin = A0; // Analog pin connected to Hall sensor
-float restValue = 0.16; // Variable for rest value
-float maxValue = 0.695; // Variable for max value
+float restValue = 0.157380254154448; // Variable for rest value
+float maxValue = 0.697947214076246; // Variable for max value
 
 // Function to map ADC value to a range between 0 and 1
 float mapToRange(int adcValue, int adcMaxValue) {
@@ -13,13 +13,10 @@ void loop() {
     // Read the ADC value from the Hall sensor
     int adcValue = analogRead(hallSensorPin);
     // Map the ADC value to a range between 0 and 1
-    float pedalValue = mapToRange(adcValue, 1023) - restValue;
-    // Adjusted value
-    float adjustedPedalValue = pedalValue / maxValue;
-    
+    float pedalValue = (mapToRange(adcValue, 1023) - restValue) / maxValue;
     // Print the pedal value to the Serial Monitor
     Serial.print("Pedal Value: ");
-    Serial.println(adjustedPedalValue, 2); // Print with 2 decimal places
+    Serial.println(pedalValue, 2); // Print with 2 decimal places
     
     delay(500); // Delay for half a second
 }
