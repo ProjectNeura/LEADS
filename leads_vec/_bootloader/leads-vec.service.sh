b6d3004a26e4e47cc3d38124dc5a1096c51b4613
Terry Fu <futerry@outlook.com>
2024-07-30 23:36:19 +0800

    Using `leads-vec` everywhere. (#328) (#329)

--- leads_vec/_bootloader/leads-vec.service.sh
+++ leads_vec/_bootloader/leads-vec.service.sh
@@ -5,5 +5,5 @@ then
   printf "Error: Config file does not exist or not readable\n" >&2
   exit 1
 fi
-# change the interpreter or adjust the arguments according to your needs
-python-leads -m leads_vec -c /usr/local/leads/config.json run
+# adjust the arguments according to your needs
+leads-vec -c /usr/local/leads/config.json run
