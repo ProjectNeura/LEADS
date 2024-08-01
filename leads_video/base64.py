27eb5df5f3f14de30ff3f669eb5221c4b8d19210
Terry Fu <futerry@outlook.com>
2024-08-01 20:46:56 +0800

    Support Extracting the Visual Data in a CSV File to MP4 (#343)
    * Optimized dependency chain. (#339)
    
    * Added `extract_video()`. (#339)

--- leads_video/base64.py
+++ leads_video/base64.py
@@ -1,10 +1,9 @@
-from base64 import b64encode as _b64encode, b64decode as _b64decode
+from base64 import b64encode as _b64encode
 from io import BytesIO as _BytesIO
 from typing import Literal as _Literal
 
-from PIL.Image import Image as _Image, fromarray as _fromarray, open as _open, \
-    UnidentifiedImageError as _UnidentifiedImageError
-from numpy import ndarray as _ndarray, array as _array
+from PIL.Image import Image as _Image, fromarray as _fromarray
+from numpy import ndarray as _ndarray
 
 
 def encode_image(x: _ndarray | None, mode: _Literal["L", "RGB"] | None = None) -> _Image | None:
@@ -12,18 +11,7 @@ def encode_image(x: _ndarray | None, mode: _Literal["L", "RGB"] | None = None) -
 
 
 def base64_encode(x: _ndarray | None, mode: _Literal["L", "RGB"] | None = None, quality: int = 25) -> str:
-    if not (img := encode_image(x, mode)):
+    if x is None:
         return ""
-    img.save(buffer := _BytesIO(), "JPEG", quality=quality)
+    _fromarray(x.transpose(1, 2, 0), mode).save(buffer := _BytesIO(), "JPEG", quality=quality)
     return _b64encode(buffer.getvalue()).decode()
-
-
-def base64_decode_image(x_base64: str) -> _Image | None:
-    try:
-        return _open(_BytesIO(_b64decode(x_base64))) if x_base64 else None
-    except (ValueError, TypeError, _UnidentifiedImageError):
-        return
-
-
-def base64_decode(x_base64: str) -> _ndarray | None:
-    return _array(image).transpose(2, 0, 1) if (image := base64_decode_image(x_base64)) else None
---
+++ leads_video/types.py
@@ -0,0 +1,3 @@
+from typing import Literal as _Literal
+
+type VideoTag = _Literal["front_view_base64", "left_view_base64", "right_view_base64", "rear_view_base64"]
