99b45d11f429cf31215cf72415b2e7250b245d2f
ATATC <futerry@outlook.com>
2024-08-01 15:36:41 +0800

    Added `extract_video()`. (#339)

--- leads_video/utils.py
+++ leads_video/utils.py
@@ -1,5 +1,17 @@
+from base64 import b64decode as _b64decode
+from binascii import Error as _BinasciiError
+from io import BytesIO as _BytesIO
+from typing import Any as _Any
+
+from PIL.Image import Image as _Image, open as _open, UnidentifiedImageError as _UnidentifiedImageError
+from cv2 import VideoWriter as _VideoWriter, VideoWriter_fourcc as _VideoWriter_fourcc, cvtColor as _cvtColor, \
+    COLOR_RGB2BGR as _COLOR_RGB2BGR
+from numpy import array as _array
+
 from leads import has_device as _has_device, get_device as _get_device
+from leads.data_persistence import CSVDataset as _CSVDataset
 from leads_video.camera import Camera
+from leads_video.types import VideoTag as _VideoTag
 
 
 def get_camera(tag: str, required_type: type[Camera] = Camera) -> Camera | None:
@@ -9,3 +21,40 @@ def get_camera(tag: str, required_type: type[Camera] = Camera) -> Camera | None:
     if not isinstance(cam, required_type):
         raise TypeError(f"Device \"{tag}\" is supposed to be a camera")
     return cam
+
+
+def _decode_frame(row: dict[str, _Any], tag: _VideoTag) -> _Image:
+    if not (frame := row[tag]):
+        raise ValueError
+    return _open(_BytesIO(_b64decode(frame)))
+
+
+def extract_video(file: str, dataset: _CSVDataset, tag: _VideoTag) -> None:
+    if not file.endswith(".mp4"):
+        file += ".mp4"
+    prev_row = None
+    resolution = None
+    fps = 0
+    cache = None
+    for row in dataset:
+        if not resolution:
+            try:
+                frame = _decode_frame(row, tag)
+                cache = _cvtColor(_array(frame), _COLOR_RGB2BGR)
+                resolution = _decode_frame(row, tag).size
+            except (ValueError, _BinasciiError, _UnidentifiedImageError):
+                pass
+        if prev_row:
+            local_fps = 1000 / (row["t"] - prev_row["t"])
+            if local_fps > fps:
+                fps = local_fps
+        prev_row = row
+    if not resolution or not fps or cache is None:
+        raise AttributeError("Failed to determine video resolution, frame rate, or cache")
+    writer = _VideoWriter(file, _VideoWriter_fourcc(*"mp4v"), fps, resolution)
+    for row in dataset:
+        try:
+            writer.write(cache := _cvtColor(_array(_decode_frame(row, tag)), _COLOR_RGB2BGR))
+        except (ValueError, _BinasciiError, _UnidentifiedImageError):
+            writer.write(cache)
+    writer.release()
