8dc6f9bcff597db5eef332ea78286cb1bc02375e
ATATC <futerry@outlook.com>
2024-08-05 07:32:49 +0200

    Supported cameras when replaying. (#338)

--- leads_vec/replay.py
+++ leads_vec/replay.py
@@ -1,8 +1,24 @@
-from leads import controller, MAIN_CONTROLLER, require_config, DataContainer
-from leads.data_persistence import CSVDataset
-from leads_emulation.replay import ReplayController
+from leads import controller, MAIN_CONTROLLER, require_config, VisualDataContainer, DataContainer, device, \
+    FRONT_VIEW_CAMERA, LEFT_VIEW_CAMERA, RIGHT_VIEW_CAMERA, REAR_VIEW_CAMERA
+from leads.data_persistence import CSVDataset, VISUAL_HEADER_ONLY
+from leads_emulation.replay import ReplayController, ReplayCamera
+from leads_vec.config import Config
+
+config: Config = require_config()
+CAMERA_RESOLUTION: tuple[int, int] | None = config.get("camera_resolution")
+dataset: CSVDataset = CSVDataset(f"{config.data_dir}/main.csv")
+visual: bool = set(VISUAL_HEADER_ONLY).issubset(dataset.read_header())
 
 
-@controller(MAIN_CONTROLLER, args=(CSVDataset(f"{require_config().data_dir}/main.csv"), DataContainer))
+@controller(MAIN_CONTROLLER, args=(dataset, VisualDataContainer if visual else DataContainer))
 class MainController(ReplayController[DataContainer]):
     pass
+
+
+if visual:
+    @device((FRONT_VIEW_CAMERA, LEFT_VIEW_CAMERA, RIGHT_VIEW_CAMERA, REAR_VIEW_CAMERA), MAIN_CONTROLLER, [
+        ("front", CAMERA_RESOLUTION), ("left", CAMERA_RESOLUTION), ("right", CAMERA_RESOLUTION),
+        ("rear", CAMERA_RESOLUTION)
+    ])
+    class Cameras(ReplayCamera):
+        pass
