3bdf2c1d0982d25f4f91c8d4b640a1f6e9969ab2
Terry Fu <futerry@outlook.com>
2024-08-05 16:18:27 +0800

    Cameras do not Exist when Replaying (#347)
    * Fixed profile. (#340)
    
    * Code reformatted. (#338)
    
    * Added `EmulatedCamera`. (#338)
    
    * Renamed `EmulatedCamera` to `ReplayCamera`. (#338)
    
    * Added `data_dir` in `leads_vec`'s config. (#338)
    
    * Bug fixed: should not initialize parent. (#338)
    
    * Bug fixed: should not close parent. (#338)
    
    * Bug fixed: `read()` is never triggered. (#338)
    
    * Supported cameras when replaying. (#338)

--- leads_emulation/replay.py
+++ leads_emulation/replay.py
@@ -1,8 +1,15 @@
+from base64 import b64decode as _b64decode
+from io import BytesIO as _BytesIO
 from typing import override as _override, Iterator as _Iterator, Any as _Any, TypeVar as _TypeVar, \
-    Generic as _Generic
+    Generic as _Generic, Literal as _Literal
 
-from leads import Controller as _Controller, DataContainer as _DataContainer
+from PIL.Image import open as _open, Image as _Image
+from numpy import ndarray as _ndarray, array as _array
+
+from leads import Controller as _Controller, DataContainer as _DataContainer, get_controller as _get_controller, \
+    VisualDataContainer as _VisualDataContainer, Device as _Device
 from leads.data_persistence import CSVDataset as _CSVDataset
+from leads_video import Camera as _Camera
 
 T = _TypeVar("T", bound=_DataContainer)
 
@@ -13,6 +20,7 @@ class ReplayController(_Controller, _Generic[T]):
         self._dataset: _CSVDataset = dataset
         self._constructor: type[T] = data_container_constructor
         self._iterator: _Iterator[dict[str, _Any]] = iter(dataset)
+        self._current_data_container: T | None = None
 
     @_override
     def read(self) -> T:
@@ -22,9 +30,42 @@ class ReplayController(_Controller, _Generic[T]):
             return self._constructor()
         t = d.pop("t")
         dc = self._constructor(**d)
-        dc._time_stamp = t
+        setattr(dc, "_time_stamp", t)
+        self._current_data_container = dc
         return dc
+
+    def current_data_container(self) -> T | None:
+        return self._current_data_container
 
     @_override
     def close(self) -> None:
         self._dataset.close()
+
+
+class ReplayCamera(_Camera):
+    def __init__(self, channel: _Literal["front", "left", "right", "rear"],
+                 resolution: tuple[int, int] | None = None) -> None:
+        super().__init__(-1, resolution)
+        self._channel: _Literal["front", "left", "right", "rear"] = channel
+        self._controller: ReplayController | None = None
+
+    @_override
+    def initialize(self, *parent_tags: str) -> None:
+        _Device.initialize(self, *parent_tags)
+        if not isinstance(controller := _get_controller(parent_tags[-1]), ReplayController):
+            raise TypeError("Emulated cameras must be initialized with a replay controller")
+        self._controller = controller
+
+    @_override
+    def read(self) -> _ndarray | None:
+        return _array(self.read_pil())
+
+    @_override
+    def read_pil(self) -> _Image | None:
+        if not isinstance(dc := self._controller.current_data_container(), _VisualDataContainer):
+            raise TypeError("Emulated cameras require visual data containers")
+        return _open(_BytesIO(_b64decode(getattr(dc, f"{self._channel}_view_base64"))))
+
+    @_override
+    def close(self) -> None:
+        pass
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -21,6 +21,6 @@ execute_root "/bin/bash" "python-install.sh"
 echo "Cleaning up..."
 execute_root "rm" "python-install.sh"
 echo "Installing dependencies..."
-execute_root "pip-leads" "install" '"leads[standard]"'
+execute_root "pip-leads" "install" '"leads[vec]"'
 echo "Verifying..."
 execute "leads-vec" "info"
