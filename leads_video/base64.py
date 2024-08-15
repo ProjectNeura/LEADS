9576ddffae30f38fb54c421da0238744db96ee67
Terry Fu <futerry@outlook.com>
2024-08-15 18:53:18 -0400

    Removed all transposition. (#352) (#360)

--- leads_video/base64.py
+++ leads_video/base64.py
@@ -7,11 +7,11 @@ from numpy import ndarray as _ndarray
 
 
 def encode_image(x: _ndarray | None, mode: _Literal["L", "RGB"] | None = None) -> _Image | None:
-    return None if x is None else _fromarray(x.transpose(1, 2, 0), mode)
+    return None if x is None else _fromarray(x, mode)
 
 
 def base64_encode(x: _ndarray | None, mode: _Literal["L", "RGB"] | None = None, quality: int = 25) -> str:
     if x is None:
         return ""
-    _fromarray(x.transpose(1, 2, 0), mode).save(buffer := _BytesIO(), "JPEG", quality=quality)
+    _fromarray(x, mode).save(buffer := _BytesIO(), "JPEG", quality=quality)
     return _b64encode(buffer.getvalue()).decode()
