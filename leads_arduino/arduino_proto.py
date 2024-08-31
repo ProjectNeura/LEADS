cdc3e4df498feba73840b747bd89973c310ae7bc
ATATC <futerry@outlook.com>
2024-08-30 20:41:33 -0400

    Generalized auto identity. (#362)

--- leads_arduino/arduino_proto.py
+++ leads_arduino/arduino_proto.py
@@ -1,15 +1,13 @@
 from typing import override as _override, Literal as _Literal
 
 from serial import Serial as _Serial
-from serial.serialutil import SerialException
-from serial.tools.list_ports import comports as _comports
 
 from leads import Controller as _Controller, SFT as _SFT, L as _L
 from leads.comm import Entity as _Entity, Callback as _Callback, Service as _Service
-from leads_comm_serial import SerialConnection as _SerialConnection
+from leads_comm_serial import SerialConnection as _SerialConnection, AutoIdentity as _AutoIdentity
 
 
-class ArduinoProto(_Controller, _Entity):
+class ArduinoProto(_Controller, _Entity, _AutoIdentity):
     """
     Supports:
     - Any arduino connected through a USB (serial) port
@@ -18,11 +16,11 @@ class ArduinoProto(_Controller, _Entity):
     def __init__(self, port: str | _Literal["auto"], baud_rate: int = 9600) -> None:
         _Controller.__init__(self)
         _Entity.__init__(self, -1, _ArduinoCallback(self))
+        _AutoIdentity.__init__(self, port == "auto")
         self._serial: _Serial = _Serial()
-        self._port_list: list[str] | None = [p[0] for p in _comports()] if port == "auto" else None
-        self._serial.port = self._port_list.pop(0) if port == "auto" else port
         self._serial.baudrate = baud_rate
         self._connection: _SerialConnection | None = None
+        self._serial.port = self.suggest_next_port() if port == "auto" else port
 
     @_override
     def port(self) -> str:
@@ -39,22 +37,16 @@ class ArduinoProto(_Controller, _Entity):
             d.update(data)
 
     @_override
+    def check_identity(self, connection: _SerialConnection) -> bool:
+        connection.send(b"ic")
+        return (msg := connection.receive()) is None or not msg.startswith(self.tag().encode())
+
+    @_override
     def run(self) -> None:
-        try:
-            self._callback.on_initialize(self)
-            self._serial.open()
-            self._connection.send(b"ic")
-            msg = self._connection.receive()
-            if msg is None or not msg.startswith(self.tag().encode()):
-                raise ConnectionError("Unmatched identity")
-            self._callback.on_connect(self, connection := _SerialConnection(self, self._serial, self._serial.port))
-            self._connection = connection
-            self._stage(connection)
-        except (SerialException, ConnectionError):
-            if not self._port_list:
-                raise ConnectionError("Connection failed")
-            self._serial.port = self._port_list.pop(0)
-            self.run()
+        self._callback.on_initialize(self)
+        self._connection = self.establish_connection(self, self._serial)
+        self._callback.on_connect(self, self._connection)
+        self._stage(self._connection)
 
     @_override
     def write(self, payload: bytes) -> None:
---
+++ leads_comm_serial/identity.py
@@ -0,0 +1,35 @@
+from abc import ABCMeta as _ABCMeta, abstractmethod as _abstractmethod
+
+from serial import Serial as _Serial, SerialException as _SerialException
+from serial.tools.list_ports import comports as _comports
+
+from leads.comm import Service as _Service
+from leads_comm_serial.connection import SerialConnection
+
+
+class AutoIdentity(object, metaclass=_ABCMeta):
+    def __init__(self, retry: bool = False) -> None:
+        self._retry: bool = retry
+        self._tried_ports: list[str] = []
+
+    def suggest_next_port(self, tried_port: str | None = None) -> str:
+        if tried_port:
+            self._tried_ports.append(tried_port)
+        for port, _, __ in _comports():
+            if port not in self._tried_ports:
+                return port
+        raise ConnectionError("No available port")
+
+    @_abstractmethod
+    def check_identity(self, connection: SerialConnection) -> bool:
+        raise NotImplementedError
+
+    def establish_connection(self, service: _Service, serial: _Serial) -> SerialConnection:
+        try:
+            serial.open()
+            if self.check_identity(connection := SerialConnection(service, serial, serial.port)):
+                return connection
+            raise ValueError("Unexpected identity")
+        except (_SerialException, ConnectionError, ValueError):
+            serial.port = self.suggest_next_port(serial.port)
+            return self.establish_connection(service, serial)
