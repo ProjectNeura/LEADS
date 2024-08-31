6de4d393cbf191cc74af80d77098433a1d140e80
ATATC <futerry@outlook.com>
2024-08-30 22:26:24 -0400

    Finished the template. (#368)

--- leads/plugin/atbs.py
+++ leads/plugin/atbs.py
@@ -1,6 +1,19 @@
+from typing import override as _override, Any as _Any
+
+from leads.constant import SystemLiteral
+from leads.context import Context
+from leads.event import InterventionEvent, InterventionExitEvent
 from leads.plugin.plugin import ESCPlugin
+
+
+def do_atbs(context: Context) -> InterventionEvent:
+    return InterventionExitEvent(context, SystemLiteral.ATBS)
 
 
 class ATBS(ESCPlugin):
     def __init__(self) -> None:
-        super().__init__(("front_wheel_speed",))
+        super().__init__()
+
+    @_override
+    def pre_update(self, context: Context, kwargs: dict[str, _Any]) -> None:
+        context.intervene(do_atbs(context))
