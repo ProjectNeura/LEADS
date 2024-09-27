15ffb289e58d47bcad3ec5c2bf850521fc48b3a2
ATATC <futerry@outlook.com>
2024-09-27 18:56:27 -0400

    Added `linear()`. (#403)

--- leads_arduino/accelerometer.py
+++ leads_arduino/accelerometer.py
@@ -1,7 +1,15 @@
 from dataclasses import dataclass as _dataclass
-from typing import override as _override
+from typing import override as _override, Self as _Self
+
+from numpy import ndarray as _ndarray, sin as _sin, cos as _cos, array as _array
 
 from leads import Device as _Device, Serializable as _Serializable
+
+
+def rotation_matrix(yaw: float, pitch: float, roll: float) -> _ndarray:
+    sy, cy, sp, cp, sr, cr = _sin(yaw), _cos(yaw), _sin(pitch), _cos(pitch), _sin(roll), _cos(roll)
+    return _array([[cy, -sy, 0], [sy, cy, 0], [0, 0, 1]]) @ _array([[cp, 0, sp], [0, 1, 0], [-sp, 0, cp]]) @ _array(
+        [[1, 0, 0], [0, cr, -sr], [0, sr, cr]])
 
 
 @_dataclass
@@ -12,6 +20,11 @@ class Acceleration(_Serializable):
     forward_acceleration: float
     lateral_acceleration: float
     vertical_acceleration: float
+
+    def linear(self) -> _Self:
+        fg = rotation_matrix(self.yaw, self.pitch, self.roll).T @ [0, 0, -9.8]
+        return Acceleration(self.yaw, self.pitch, self.roll, self.forward_acceleration + fg[0],
+                            self.lateral_acceleration + fg[1], self.vertical_acceleration + fg[2])
 
 
 class Accelerometer(_Device):
