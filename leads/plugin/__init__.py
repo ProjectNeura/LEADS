060270d37b99a989ada83cf97f85bb6d8f3d9f12
ATATC <futerry@outlook.com>
2024-09-30 11:36:17 -0400

    Removed `GPSSpeedCorrection`. (#409)

--- leads/plugin/__init__.py
+++ leads/plugin/__init__.py
@@ -2,5 +2,4 @@ from leads.plugin.abs import *
 from leads.plugin.atbs import *
 from leads.plugin.dtcs import *
 from leads.plugin.ebi import *
-from leads.plugin.gps_speed_correction import *
 from leads.plugin.plugin import *
