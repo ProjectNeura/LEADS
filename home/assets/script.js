f35801cdfad8bce2d3b0c9d9c96a4a885ea8bc11
Tianhao (Terry) Fu <futerry@outlook.com>
2024-10-22 13:18:25 -0400

    Update the Home Page (#446)
    * Docs. (#394)
    
    * Docs. (#394)
    
    * Docs. (#394)

--- home/assets/script.js
+++ home/assets/script.js
@@ -4,7 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
     let currentIndex = 0;
 
     function showLine(index) {
-        if (index < 1) background.style.background = "#343751";
+        if (index === 0 || index === 5 || index === 8) background.style.background = "#343751";
+        else if (index === 7) background.style.background = "#000000";
         else background.style.background = "url(\"assets/background-" + index + ".png\") no-repeat center center/cover";
         lines.forEach((line, i) => {
             line.classList.remove("visible", "exit");
