bc72c59648a0a3f83658fbe1ffa1a4e579c9a15e
ATATC <futerry@outlook.com>
2025-01-31 10:02:22 -0500

    Added import restrictions. (#464)

--- leads_national_instruments/__init__.py
+++ leads_national_instruments/__init__.py
@@ -3,4 +3,9 @@ from importlib.util import find_spec as _find_spec
 if not _find_spec("nidaqmx"):
     raise ImportError("Please install `nidaqmx` to run this module\n>>>pip install nidaqmx")
 
+from leads_gui.system import get_system_kernel as _get_system_kernel
+
+if _get_system_kernel() != "windows":
+    raise ImportError("National Instruments bindings for Python only supports Windows environments")
+
 from leads_national_instruments.mydaq import *
