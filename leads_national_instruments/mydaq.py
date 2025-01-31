9e0b555c4c6740d67b95576241d80ca935cfb361
ATATC <futerry@outlook.com>
2025-01-31 10:10:11 -0500

    Added import restrictions. (#464)

--- leads_national_instruments/mydaq.py
+++ leads_national_instruments/mydaq.py
@@ -1,6 +1,10 @@
 from typing import override as _override
 
-from nidaqmx import Task as _Task
+try:
+    from nidaqmx import Task as _Task
+except ImportError as _e:
+    raise ImportError(
+        "If `nidaqmx` is installed, check if either NI-DAQmx or NI-DAQmx Runtime is not installed") from _e
 
 from leads import Device as _Device
 
