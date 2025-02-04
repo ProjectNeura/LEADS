0934bafb7b1d865fb14563fe7e463f7fc0c6068d
ATATC <futerry@outlook.com>
2025-02-03 21:17:10 -0500

    Routine updates. (#464)

--- leads_national_instruments/mydaq.py
+++ leads_national_instruments/mydaq.py
@@ -6,13 +6,27 @@ except ImportError as _e:
     raise ImportError(
         "If `nidaqmx` is installed, check if either NI-DAQmx or NI-DAQmx Runtime is not installed") from _e
 
-from leads import Device as _Device
+from leads import Controller as _Controller, Device as _Device, get_controller as _get_controller
 
 
-class MyDAQ(_Device):
+class AnalogInput(_Device):
+    def __init__(self, channel: str) -> None:
+        super().__init__(channel)
+
+    @_override
+    def initialize(self, *parent_tags: str) -> None:
+        controller = _get_controller(parent_tags[-1])
+        if isinstance(controller, MyDAQ):
+            controller.task().ai_channels.add_ai_voltage_chan(self._pins[0])
+
+
+class MyDAQ(_Controller):
     def __init__(self) -> None:
         super().__init__()
         self._task: _Task = _Task()
+
+    def task(self) -> _Task:
+        return self._task
 
     @_override
     def close(self) -> None:
